2b160aede84558cb561ecb25cb6fd7b2d79f5dd4
Begum <topcuoglu.begum@gmail.com>
2018-08-10 12:27:21 -0400

    put cv and test in loop to print out one plot of mean AUC values

--- code/XGboost_baxter.py
+++ code/XGboost_baxter.py
@@ -138,6 +138,8 @@ for epoch in range(epochs):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
     X=x_train.values
     Y=y_train.values
+    X_test= x_test.values
+    Y_test= y_test.values
     i = 0
     for train, test in cv.split(X,Y):
         probas_ = clf.fit(X[train], Y[train]).predict_proba(X[test])
@@ -147,11 +149,28 @@ for epoch in range(epochs):
         tprs[-1][0] = 0.0
         roc_auc = auc(fpr, tpr)
         aucs.append(roc_auc)
-        plt.plot(fpr, tpr, lw=1, alpha=0.3, label='Epoch %d, ROC fold %d (AUC = %0.2f)' % (epoch, i, roc_auc))
-        i += 1
+        #plt.plot(fpr, tpr, lw=1, alpha=0.3, label='Epoch %d, ROC fold %d (AUC = %0.2f)' % (epoch, i, roc_auc))
+        #i += 1
+    tprs_test = []
+    aucs_test = []
+    mean_fpr_test = np.linspace(0, 1, 100)
+    epochs_test = 50
+    for epoch_test in range(epochs_test):
+        probas_ = clf.predict_proba(X_test)
+        # Compute ROC curve and area the curve
+        fpr_test, tpr_test, thresholds_test = roc_curve(Y_test, probas_[:, 1])
+        tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
+        tprs_test[-1][0] = 0.0
+        roc_auc_test = auc(fpr_test, tpr_test)
+        aucs_test.append(roc_auc_test)
 
 
 plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='Luck', alpha=.8)
+mean_tpr_test = np.mean(tprs_test, axis=0)
+mean_tpr_test[-1] = 1.0
+mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
+std_auc_test = np.std(aucs_test)
+plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Mean test ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
 mean_tpr = np.mean(tprs, axis=0)
 mean_tpr[-1] = 1.0
 mean_auc = auc(mean_fpr, mean_tpr)
@@ -168,48 +187,6 @@ plt.ylabel('True Positive Rate')
 plt.title('XGBoost ROC\n')
 plt.legend(loc="lower right", fontsize=8)
 plt.show()
-
-
-
-tprs = []
-aucs = []
-mean_fpr = np.linspace(0, 1, 100)
-
-epochs = 50
-for epoch in range(epochs):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
-    X=x_test.values
-    Y=y_test.values
-    probas_ = clf.predict_proba(X)
-    # Compute ROC curve and area the curve
-    fpr, tpr, thresholds = roc_curve(Y, probas_[:, 1])
-    tprs.append(interp(mean_fpr, fpr, tpr))
-    tprs[-1][0] = 0.0
-    roc_auc = auc(fpr, tpr)
-    aucs.append(roc_auc)
-    plt.plot(fpr, tpr, lw=1, alpha=0.3, label='Epoch %d (AUC = %0.2f)' % (epoch, roc_auc))
-
-
-plt.plot([0, 1], [0, 1], linestyle='--', color='r', label='Luck', alpha=.8)
-mean_tpr = np.mean(tprs, axis=0)
-mean_tpr[-1] = 1.0
-mean_auc = auc(mean_fpr, mean_tpr)
-std_auc = np.std(aucs)
-plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
-std_tpr = np.std(tprs, axis=0)
-tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2, label=r'$\pm$ 1 std. dev.')
-plt.xlim([-0.05, 1.05])
-plt.ylim([-0.05, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('XGBoost ROC\n')
-plt.legend(loc="lower right", fontsize=8)
-plt.show()
-
-
-
 
 
 
