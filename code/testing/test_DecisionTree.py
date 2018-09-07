7ec421ceb27c427aa58d73ffdbf35f54e54096b5
Begum <topcuoglu.begum@gmail.com>
2018-09-07 11:26:05 -0400

    initial commit

---
+++ code/testing/test_DecisionTree.py
@@ -0,0 +1,114 @@
+############## IMPORT MODULES ######################
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sympy import *
+import matplotlib.pyplot as plt
+from scipy import interp
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.externals import joblib
+
+
+## Read in the data
+
+## shared has our input features(OTUs)
+shared = pd.read_table("data/baxter.0.03.subsample.shared")
+## meta has the labels(diagnosis)
+meta = pd.read_table("data/metadata.tsv")
+
+## Check and visualize the data
+meta.head()
+shared.head()
+
+## Remove unnecessary columns from meta and only keep label for classification(diagnosis) and the sample name
+meta = meta[['sample','dx']]
+
+## Rename the column name "Group" to match the "sample" in meta
+shared = shared.rename(index=str, columns={"Group":"sample"})
+
+## Merge the 2 datasets on sample
+data=pd.merge(meta,shared,on=['sample'])
+
+## Remove adenoma samples. We will do a 2 classification model, just for cancer and normal colon samples.
+data= data[data.dx.str.contains("adenoma") == False]
+
+## Drop all except OTU columns for x
+x = data.drop(["sample", "dx", "numOtus", "label"], axis=1)
+
+## Cancer =1 Normal =0
+diagnosis = { "cancer":1, "normal":0}
+
+## Generate y which only has diagnosis as 0 and 1
+y = data["dx"].replace(diagnosis)
+# y = np.eye(2, dtype='uint8')[y]
+
+## Drop if NA elements
+y.dropna()
+x.dropna()
+
+
+cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=100, random_state=200889)
+
+
+###################### Best Parameters #######################
+## Best accuracy : 0.694776 (0.070054) with: {'max_depth': 100, 'min_samples_split': 100}
+##############################################################
+
+tprs_test = []
+aucs_test = []
+mean_fpr_test = np.linspace(0, 1, 100)
+#SVM_plot = plt.figure()
+
+epochs= 100
+for epoch in range(epochs):
+    ## Split dataset to 80% training 20% test sets.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
+
+    best_model = DecisionTreeClassifier(C=1.0, kernel='sigmoid', gamma=0.001)
+
+## Converting to numpy array from pandas
+
+## Converting to numpy array from pandas
+
+    x_test= x_test.values
+    y_test= y_test.values
+
+
+    y_score = best_model.fit(x_train, y_train).decision_function(x_test)
+
+    # Compute ROC curve and area the curve
+    fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_score)
+    tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
+    tprs_test[-1][0] = 0.0
+    roc_auc_test = auc(fpr_test, tpr_test)
+    aucs_test.append(roc_auc_test)
+    print("Test", roc_auc_test)
+
+plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Luck', alpha=.8)
+mean_tpr_test = np.mean(tprs_test, axis=0)
+mean_tpr_test[-1] = 1.0
+mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
+std_auc_test = np.std(aucs_test)
+plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Never-before-seen test set ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
+std_tpr_test = np.std(tprs_test, axis=0)
+tprs_upper_test = np.minimum(mean_tpr_test + std_tpr_test, 1)
+tprs_lower_test = np.maximum(mean_tpr_test - std_tpr_test, 0)
+plt.fill_between(mean_fpr_test, tprs_lower_test, tprs_upper_test, color='tomato', alpha=.2, label=r'$\pm$ 1 std. dev.')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('SVM ROC\n')
+plt.legend(loc="lower right", fontsize=8)
+plt.show()
+#SVM_plot.savefig('results/figures/SVM_Baxter.png', dpi=1000)
+
+###### SAVE MODEL TO BE USED ON OTHER DATA #########
+filename = 'finalized_SVM_model.sav'
+joblib.dump(best_model, filename)
