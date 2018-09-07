90a35c16eec98a615988e8fa6726572a938fb0ca
Begum <topcuoglu.begum@gmail.com>
2018-09-07 15:44:38 -0400

    name of graph fixed

--- code/learning/DecisionTree_baxter.py
+++ code/learning/DecisionTree_baxter.py
@@ -62,7 +62,7 @@ cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=100, random_state=200889)
 model = DecisionTreeClassifier()
 
 ## Define the hyper-parameters optimization on training set.
-params = dict(max_depth=[1, 10, 50, 100], min_samples_split=[5, 10, 25, 50, 100])
+params = dict(max_depth=[5, 10, 50], min_samples_split=[10, 25, 50])
 
 grid = GridSearchCV(estimator = model, param_grid = params, cv = cv, scoring = 'roc_auc')
 
@@ -80,3 +80,77 @@ for mean, stdev, param in zip(means, stds, params):
 ###################### Best Parameters #######################
 
 ##############################################################
+best_model = grid_result.best_estimator_
+
+## Generate empty lists to fill with AUC values for test-set
+tprs_test = []
+aucs_test = []
+mean_fpr_test = np.linspace(0, 1, 100)
+
+########################### Random Forest #######################
+epochs= 100
+for epoch in range(epochs):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
+
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+
+
+## Converting to numpy array from pandas
+    X=x_train.values
+    Y=y_train.values
+    X_test= x_test.values
+    Y_test= y_test.values
+
+## Plot mean ROC curve for never before seen test set.
+    probas_ = best_model.predict_proba(x_test)
+# Compute ROC curve and area the curve
+    fpr_test, tpr_test, thresholds_test = roc_curve(y_test, probas_[:, 1])
+    tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
+    tprs_test[-1][0] = 0.0
+    roc_auc_test = auc(fpr_test, tpr_test)
+    aucs_test.append(roc_auc_test)
+    print("Test", roc_auc_test)
+
+
+
+## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
+    for train, test in cv.split(X,Y):
+        probas_ = best_model.fit(X[train], Y[train]).predict_proba(X[test])
+        fpr, tpr, thresholds = roc_curve(Y[test], probas_[:, 1])
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        print("Train", roc_auc)
+    ## Plot mean ROC curve for test set after each fitting of subset training set
+
+
+plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Luck', alpha=.8)
+mean_tpr_test = np.mean(tprs_test, axis=0)
+mean_tpr_test[-1] = 1.0
+mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
+std_auc_test = np.std(aucs_test)
+plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Never-before-seen test set ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
+std_tpr_test = np.std(tprs_test, axis=0)
+tprs_upper_test = np.minimum(mean_tpr_test + std_tpr_test, 1)
+tprs_lower_test = np.maximum(mean_tpr_test - std_tpr_test, 0)
+plt.fill_between(mean_fpr_test, tprs_lower_test, tprs_upper_test, color='tomato', alpha=.2, label=r'$\pm$ 1 std. dev.')
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean cross-val ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='dodgerblue', alpha=.2, label=r'$\pm$ 1 std. dev.')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Decision Tree ROC\n')
+plt.legend(loc="lower right", fontsize=8)
+plt.show()
+#RF_plot.savefig('results/figures/RF_Baxter.png', dpi=1000)
