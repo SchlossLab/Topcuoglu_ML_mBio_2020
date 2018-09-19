e2ad67efbdce3bde30ffea966def408a99f8e8df
Begum <topcuoglu.begum@gmail.com>
2018-09-19 11:29:40 -0400

    created the loop for 100 epochs

--- code/learning/SVM_linear_baxter.py
+++ code/learning/SVM_linear_baxter.py
@@ -52,32 +52,99 @@ y = data["dx"].replace(diagnosis)
 ## Drop if NA elements
 y.dropna()
 x.dropna()
+## Generate empty lists to fill with AUC values for test-set
+tprs_test = []
+aucs_test = []
+mean_fpr_test = np.linspace(0, 1, 100)
+
+epochs= 100
+for epoch in range(epochs):
+    SVM_plot = plt.figure()
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
+
+    scaler = StandardScaler().fit(x_train)
+    x_train = scaler.transform(x_train)
+
+    ## Define the n-folds for hyper-parameter optimization on training set.
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=100, random_state=200889)
+
+    ## Define L2 regularized logistic classifier
+    model = LinearSVC(penalty='l1', loss='squared_hinge', dual=False)
+
+    ## Define the hyper-parameters optimization on training set.
+    c_values = [0.01, 0.1, 1.0, 1.5, 2.0]
+    #gamma = ['auto', 0.0001, 0.001, 0.01, 0.1]
+    param_grid = dict(C=c_values)
+    grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = cv, scoring = 'roc_auc')
+    grid_result = grid.fit(x_train, y_train)
+    print('Best C:', grid_result.best_estimator_.get_params()['C'])
+    print('Best model:', grid_result.best_estimator_)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+        ## The best model we pick here will be used for predicting test set.
+    best_model = grid_result.best_estimator_
+# AUC calculation for cross validation
+## Generate empty lists to fill with AUC values for train-set cv
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+## Converting to numpy array from pandas
+    X=x_train.values
+    Y=y_train.values
+    for train, test in cv.split(X,Y):
+        probas_ = best_model.fit(X[train], Y[train]).predict_proba(X[test])
+        fpr, tpr, thresholds = roc_curve(Y[test], probas_[:, 1])
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        print("Train", roc_auc)
 
 
+    probas_ = best_model.fit(x_train, y_train).predict_proba(x_test)
+    # Compute ROC curve and area the curve
+    fpr_test, tpr_test, thresholds_test = roc_curve(y_test, probas_[:, 1])
+    tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
+    tprs_test[-1][0] = 0.0
+    roc_auc_test = auc(fpr_test, tpr_test)
+    aucs_test.append(roc_auc_test)
+    print("Test", roc_auc_test)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
 
-scaler = StandardScaler().fit(x_train)
-x_train = scaler.transform(x_train)
+## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
 
-## Define the n-folds for hyper-parameter optimization on training set.
-cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=20, random_state=200889)
+    ## Plot mean ROC curve for test set after each fitting of subset training set
 
-## Define L2 regularized logistic classifier
-model = LinearSVC(penalty='l1',verbose1=1)
 
-## Define the hyper-parameters optimization on training set.
-c_values = [0.01, 0.1, 1.0, 1.5, 2.0]
-#gamma = ['auto', 0.0001, 0.001, 0.01, 0.1]
-param_grid = dict(C=c_values)
-
-grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = cv, scoring = 'roc_auc')
-
-grid_result = grid.fit(x_train, y_train)
-
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-means = grid_result.cv_results_['mean_test_score']
-stds = grid_result.cv_results_['std_test_score']
-params = grid_result.cv_results_['params']
-for mean, stdev, param in zip(means, stds, params):
-    print("%f (%f) with: %r" % (mean, stdev, param))
+plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Luck', alpha=.8)
+mean_tpr_test = np.mean(tprs_test, axis=0)
+mean_tpr_test[-1] = 1.0
+mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
+std_auc_test = np.std(aucs_test)
+plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Never-before-seen test set ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
+std_tpr_test = np.std(tprs_test, axis=0)
+tprs_upper_test = np.minimum(mean_tpr_test + std_tpr_test, 1)
+tprs_lower_test = np.maximum(mean_tpr_test - std_tpr_test, 0)
+plt.fill_between(mean_fpr_test, tprs_lower_test, tprs_upper_test, color='tomato', alpha=.2, label=r'$\pm$ 1 std. dev.')
+mean_tpr = np.mean(tprs, axis=0)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+std_auc = np.std(aucs)
+plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean cross-val ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+std_tpr = np.std(tprs, axis=0)
+tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='dodgerblue', alpha=.2, label=r'$\pm$ 1 std. dev.')
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('SVM Linear Kerne; ROC\n')
+plt.legend(loc="lower right", fontsize=8)
+#plt.show()
+SVM_plot.savefig('results/figures/SVM_Linear_Baxter.png', dpi=1000)
