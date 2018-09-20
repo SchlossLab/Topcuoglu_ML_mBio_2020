c53301761d08d7a4fa1b12306931f53089db9f43
Begum <topcuoglu.begum@gmail.com>
2018-09-20 14:17:35 -0400

    iteration count

--- code/learning/LogisticRegression.py
+++ code/learning/LogisticRegression.py
@@ -68,8 +68,10 @@ mean_fpr_test = np.linspace(0, 1, 100)
 # For each epoch, we will also report mean AUC values +/- sd for each cross-validation during training.
 
 Logit_plot = plt.figure()
+i=0
 epochs= 100
 for epoch in range(epochs):
+    print(i+1)
     ## Split dataset to 80% training 20% test sets.
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
     sc = StandardScaler()
