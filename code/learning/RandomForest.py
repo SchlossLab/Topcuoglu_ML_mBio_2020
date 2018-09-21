fbea042bc6a0619db3e0dbb59766f31f688ee8b2
Begum <topcuoglu.begum@gmail.com>
2018-09-21 14:30:17 -0400

    fixed pipeline

--- code/learning/RandomForest.py
+++ code/learning/RandomForest.py
@@ -1,3 +1,7 @@
+############## IMPORT MODULES ######################
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np # linear algebra
@@ -9,6 +13,7 @@ from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import GridSearchCV
 from sklearn.externals import joblib
+from sklearn.preprocessing import StandardScaler
 
 
 ############## PRE-PROCESS DATA ######################
@@ -55,64 +60,73 @@ tprs_test = []
 aucs_test = []
 mean_fpr_test = np.linspace(0, 1, 100)
 
-########################### Random Forest #######################
-epochs= 10
+################## Random Forest ###############
+
+## We will split the dataset 80%-20% and tune hyper-parameter on the 80% training. This will be done 100 times wth 5 folds and an optimal hyper-parameter/optimal model will be chosen.
+
+## The chosen best model and hyper-parameter will be tested on the %20 test set that was not seen before during training. This will give a TEST AUC.
+
+## We will split and redo previous steps 100 epochs. Which means we have 100 models that we test on the 20%. We will report the mean TEST AUC +/- sd.
+
+# For each epoch, we will also report mean AUC values +/- sd for each cross-validation during training.
+
+RF_plot = plt.figure()
+i=0
+epochs= 50
 for epoch in range(epochs):
+    i=i+1
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2,shuffle=True)
 
 # Decide on the number of decision trees
     #param_grid = {'n_estimators': [ 25, 50, 100, 120, 150, 300, 500, 800, 1000], "max_depth": [ 5, 8, 15, 25, 30, None],'max_features': ['auto', 'sqrt', 'log2', None, 0.8], 'criterion': ["entropy", "gini"]
      #}
-    param_grid = {'n_estimators': [ 25, 50, 100, 120, 150, 300, 500, 1000]}
+    n_estimators = [5, 10, 20, 25, 30, 50, 100]
+    param_grid = dict(n_estimators=n_estimators)
      ## Define the n-folds for hyper-parameter optimization on training set.
-    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=10, random_state=200889)
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=50, random_state=200889)
     #use out-of-bag samples ("oob_score= True") to estimate the generalization accuracy.
-    rfc = RandomForestClassifier(bootstrap= True, n_jobs= 1)
+    model = RandomForestClassifier(bootstrap= True)
     #let's use cv=10 in the GridSearchCV call
     #performance estimation
     #initiate the grid
-    grid = GridSearchCV(rfc, param_grid = param_grid, cv=cv, scoring ='roc_auc')
-    #fit your data before you can get the best parameter combination.
-    grid.fit(x_train,y_train)
-    best_model = grid.fit(x_train,y_train)
-    best_model = best_model.best_estimator_
-    print('Best model:', best_model.best_estimator_)
-
+    grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = cv, scoring = 'roc_auc', n_jobs=-1)
+    grid_result = grid.fit(x_train, y_train)
+    print('Best C:', grid_result.best_estimator_.get_params()['C'])
+    print('Best model:', grid_result.best_estimator_)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+        ## The best model we pick here will be used for predicting test set.
+        best_model = grid_result.best_estimator_
+        ## Generate empty lists to fill with AUC values for train-set cv
     tprs = []
     aucs = []
     mean_fpr = np.linspace(0, 1, 100)
+    ## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
+    for train, test in cv.split(X,Y):
+        y_score = best_model.fit(X[train], Y[train]).decision_function(X[test])
+        fpr, tpr, thresholds = roc_curve(Y[test], y_score)
+        tprs.append(interp(mean_fpr, fpr, tpr))
+        tprs[-1][0] = 0.0
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        print("Train", roc_auc)
 
-    RF_plot = plt.figure()
-
-## Converting to numpy array from pandas
-    X=x_train.values
-    Y=y_train.values
-    X_test= x_test.values
-    Y_test= y_test.values
-
-## Plot mean ROC curve for never before seen test set.
-    probas_ = best_model.predict_proba(x_test)
-# Compute ROC curve and area the curve
-    fpr_test, tpr_test, thresholds_test = roc_curve(y_test, probas_[:, 1])
+    ## Plot mean ROC curve for 100 epochs test set evaulation.
+    y_score_test = best_model.fit(x_train, y_train).decision_function(x_test)
+    # Compute ROC curve and area the curve
+    fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_score_test)
     tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
     tprs_test[-1][0] = 0.0
     roc_auc_test = auc(fpr_test, tpr_test)
     aucs_test.append(roc_auc_test)
     print("Test", roc_auc_test)
 
-## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
-    for train, test in cv.split(X,Y):
-        probas_ = best_model.fit(X[train], Y[train]).predict_proba(X[test])
-        fpr, tpr, thresholds = roc_curve(Y[test], probas_[:, 1])
-        tprs.append(interp(mean_fpr, fpr, tpr))
-        tprs[-1][0] = 0.0
-        roc_auc = auc(fpr, tpr)
-        aucs.append(roc_auc)
-        print("Train", roc_auc)
-    ## Plot mean ROC curve for test set after each fitting of subset training set
-
-
-plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Luck', alpha=.8)
+plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Random', alpha=.8)
 mean_tpr_test = np.mean(tprs_test, axis=0)
 mean_tpr_test[-1] = 1.0
 mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
@@ -138,9 +152,4 @@ plt.ylabel('True Positive Rate')
 plt.title('Random Forest ROC\n')
 plt.legend(loc="lower right", fontsize=8)
 #plt.show()
-RF_plot.savefig('results/figures/RF_Baxter.png', dpi=1000)
-
-
-###### SAVE MODEL TO BE USED ON DOTHER DATA #########
-filename = 'finalized_RF_model.sav'
-joblib.dump(best_model, filename)
+RF_plot.savefig('results/figures/Random_Forest_baxter.png', dpi=1000)
