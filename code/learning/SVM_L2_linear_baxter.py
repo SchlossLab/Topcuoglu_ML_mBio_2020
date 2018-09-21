ce8e2cfcd9f9c4d268084afe65e5b1f2cf321cd9
Begum <topcuoglu.begum@gmail.com>
2018-09-21 09:40:06 -0400

    another method backend

--- code/learning/SVM_L2_linear_baxter.py
+++ code/learning/SVM_L2_linear_baxter.py
@@ -1,4 +1,7 @@
 ############## IMPORT MODULES ######################
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from sklearn.neighbors.nearest_centroid import NearestCentroid
 from sklearn.svm import SVC, LinearSVC
 from sklearn.model_selection import cross_val_score
@@ -7,9 +10,6 @@ from sklearn.model_selection import train_test_split
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 from sympy import *
-import matplotlib as mpl
-mpl.use('TkAgg')
-import matplotlib.pyplot as plt
 from scipy import interp
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import RepeatedStratifiedKFold
