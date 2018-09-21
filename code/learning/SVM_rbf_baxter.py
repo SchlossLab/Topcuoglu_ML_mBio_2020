d8a594d1c73b5b25df989bd45650fbcccfdcbc22
Begum <topcuoglu.begum@gmail.com>
2018-09-21 09:49:22 -0400

    works with this

--- code/learning/SVM_rbf_baxter.py
+++ code/learning/SVM_rbf_baxter.py
@@ -9,7 +9,7 @@ from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-from sympy import
+from sympy import *
 from scipy import interp
 from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import RepeatedStratifiedKFold
