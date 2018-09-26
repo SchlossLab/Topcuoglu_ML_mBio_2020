13142f21a0dbc372e74a7bc415db225c0d0a3bdb
Begum <topcuoglu.begum@gmail.com>
2018-09-26 17:02:41 -0400

    remove model specific parameter prints

--- code/learning/main.py
+++ code/learning/main.py
@@ -61,9 +61,6 @@ for epoch in range(epochs):
     grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = cv, scoring = 'roc_auc', n_jobs=-1)
     ## Get the grid results and fit to training set
     grid_result = grid.fit(x_train, y_train)
-    print('Best C:', grid_result.best_estimator_.get_params()['C'])
-    print('Best model:', grid_result.best_estimator_)
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
     means = grid_result.cv_results_['mean_test_score']
     stds = grid_result.cv_results_['std_test_score']
     params = grid_result.cv_results_['params']
