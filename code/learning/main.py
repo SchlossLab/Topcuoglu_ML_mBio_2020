f3369fd6d61a2c90e1ab97450969a2f577f62a4f
Begum <topcuoglu.begum@gmail.com>
2018-09-26 17:05:00 -0400

    restored best param print

--- code/learning/main.py
+++ code/learning/main.py
@@ -61,6 +61,8 @@ for epoch in range(epochs):
     grid = GridSearchCV(estimator = model, param_grid = param_grid, cv = cv, scoring = 'roc_auc', n_jobs=-1)
     ## Get the grid results and fit to training set
     grid_result = grid.fit(x_train, y_train)
+    print('Best model:', grid_result.best_estimator_)
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
     means = grid_result.cv_results_['mean_test_score']
     stds = grid_result.cv_results_['std_test_score']
     params = grid_result.cv_results_['params']
