0c30c50a3273e5c7fa6eb2845511dd11b84b4df3
Begum <topcuoglu.begum@gmail.com>
2018-09-26 10:47:07 -0400

    initial commit

---
+++ code/test_module.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#
+# Author: Begum Topcuoglu
+# Date: 2018-09-26
+#
+# This script runs Logistic Regression analysis on Baxter Dataset subset of onlt cancer and normal samples to predict diagnosis based on OTU data only. This script only evaluates generalization performance of the model.
+#
+
+############## IMPORT MODULES ######################
+import load_modules
