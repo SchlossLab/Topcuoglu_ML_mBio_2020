9f65e095c65fa195f0491cee4f8645532699faa1
BegumTop <begumtop@umich.edu>
2018-09-27 07:54:22 -0400

    cv increase and change labels

--- code/learning/model_selection.py
+++ code/learning/model_selection.py
@@ -20,27 +20,27 @@ from sklearn.model_selection import RepeatedStratifiedKFold
 
 def select_model(net):
     ## Define the n-folds for hyper-parameter optimization on training set.
-    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=200889)
-    if net=="logreg":
+    cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=5, random_state=200889)
+    if net=="L2 Logistic Regression":
         model = linear_model.LogisticRegression()
         ## We will try these regularization strength coefficients to optimize our model
         ## We will try these regularization strength coefficients to optimize our model
         c_values = [0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01]
         param_grid = dict(C=c_values)
-    if net=="svm_l1_linear":
+    if net=="L1 SVM Linear Kernel":
         model= LinearSVC(penalty='l1', loss='squared_hinge', dual=False)
         c_values = [5, 10, 20, 25, 30, 50, 100]
         param_grid = dict(C=c_values)
-    if net=="svm_l2_linear":
+    if net=="L2 SVM Linear Kernel":
         model = SVC(kernel='linear')
         c_values = [5, 10, 20, 25, 30, 50, 100]
         param_grid = dict(C=c_values)
-    if net=="svm_rbf":
+    if net=="SVM RBF":
         model = SVC(kernel='rbf')
         c_values = [0.000001, 0.00001, 0.0001, 0.001, 0.01]
         gamma = [0.00000001, 0.0000001, 0.000001]
         param_grid = dict(C=c_values, gamma=gamma)
-    if net=="rf":
+    if net=="Random Forest":
         model = RandomForestClassifier(bootstrap= True)
         n_estimators = [500, 1000, 1250, 1500, 2000, 2500, 3000, 3500]
         param_grid = dict(n_estimators=n_estimators)
