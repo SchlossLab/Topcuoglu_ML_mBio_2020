c1253906ac237b5ab6a595281eec186f3beb5e88
BegumTop <begumtop@umich.edu>
2018-09-27 07:30:53 -0400

    fix the rf

--- code/learning/main.py
+++ code/learning/main.py
@@ -84,7 +84,7 @@ for models in models:
             ## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
 
             for train, test in cv.split(X,Y):
-                if models=="logreg" and models=="rf":
+                if models=="logreg" or models=="rf":
                     y_score = best_model.fit(X[train], Y[train]).predict_proba(X[test])
                     fpr, tpr, thresholds = roc_curve(Y[test], y_score[:, 1])
                 else:
@@ -97,15 +97,15 @@ for models in models:
                 print("Train", roc_auc)
 
         ## Plot mean ROC curve for 100 epochs test set evaulation.
-        if models=="logreg" and models=="rf":
+        if models=="logreg" or models=="rf":
             y_score = best_model.fit(x_train, y_train).predict_proba(x_test)
             # Compute ROC curve and area the curve
-            fpr_test, tpr_test, thresholds_test = roc_curve(y_test, probas_[:, 1])
+            fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_score[:, 1])
         else:
             y_score = best_model.fit(x_train, y_train).decision_function(x_test)
             # Compute ROC curve and area the curve
             fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_score)
-            
+
         tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
         tprs_test[-1][0] = 0.0
         roc_auc_test = auc(fpr_test, tpr_test)
