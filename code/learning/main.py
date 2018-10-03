145db10e3661c6f9db2c55e8d10907c0777d7eea
Begum <topcuoglu.begum@gmail.com>
2018-10-03 16:59:20 -0400

    cv loop fixed

--- code/learning/main.py
+++ code/learning/main.py
@@ -72,29 +72,29 @@ for models in models:
         params = grid_result.cv_results_['params']
         for mean, stdev, param in zip(means, stds, params):
             print("%f (%f) with: %r" % (mean, stdev, param))
-            ## The best model we pick here will be used for predicting test set.
-            best_model = grid_result.best_estimator_
-            ## Generate empty lists to fill with AUC values for train-set cv
-            tprs = []
-            aucs = []
-            mean_fpr = np.linspace(0, 1, 100)
-            ## variable assignment to make it easier to read.
-            X=x_train
-            Y=y_train
-            ## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
+        ## The best model we pick here will be used for predicting test set.
+        best_model = grid_result.best_estimator_
+        ## Generate empty lists to fill with AUC values for train-set cv
+        tprs = []
+        aucs = []
+        mean_fpr = np.linspace(0, 1, 100)
+        ## variable assignment to make it easier to read.
+        X=x_train
+        Y=y_train
+        ## Plot mean ROC curve for cross-validation with n_splits=5 and n_repeats=100 to evaluate the variation of prediction in our training set.
 
-            for train, test in cv.split(X,Y):
-                if models=="L2 Logistic Regression" or models=="Random Forest":
-                    y_score = best_model.fit(X[train], Y[train]).predict_proba(X[test])
-                    fpr, tpr, thresholds = roc_curve(Y[test], y_score[:, 1])
-                else:
-                    y_score = best_model.fit(X[train], Y[train]).decision_function(X[test])
-                    fpr, tpr, thresholds = roc_curve(Y[test], y_score)
-                tprs.append(interp(mean_fpr, fpr, tpr))
-                tprs[-1][0] = 0.0
-                roc_auc = auc(fpr, tpr)
-                aucs.append(roc_auc)
-                print("Train", roc_auc)
+        for train, test in cv.split(X,Y):
+            if models=="L2 Logistic Regression" or models=="Random Forest":
+                y_score = best_model.fit(X[train], Y[train]).predict_proba(X[test])
+                fpr, tpr, thresholds = roc_curve(Y[test], y_score[:, 1])
+            else:
+                y_score = best_model.fit(X[train], Y[train]).decision_function(X[test])
+                fpr, tpr, thresholds = roc_curve(Y[test], y_score)
+            tprs.append(interp(mean_fpr, fpr, tpr))
+            tprs[-1][0] = 0.0
+            roc_auc = auc(fpr, tpr)
+            aucs.append(roc_auc)
+            print("Train", roc_auc)
 
         ## Plot mean ROC curve for 100 epochs test set evaulation.
         if models=="L2 Logistic Regression" or models=="Random Forest":
@@ -106,36 +106,36 @@ for models in models:
             # Compute ROC curve and area the curve
             fpr_test, tpr_test, thresholds_test = roc_curve(y_test, y_score)
 
-        tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
-        tprs_test[-1][0] = 0.0
-        roc_auc_test = auc(fpr_test, tpr_test)
-        aucs_test.append(roc_auc_test)
-        print("Test", roc_auc_test)
+    tprs_test.append(interp(mean_fpr_test, fpr_test, tpr_test))
+    tprs_test[-1][0] = 0.0
+    roc_auc_test = auc(fpr_test, tpr_test)
+    aucs_test.append(roc_auc_test)
+    print("Test", roc_auc_test)
 
-        plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Random', alpha=.8)
-        mean_tpr_test = np.mean(tprs_test, axis=0)
-        mean_tpr_test[-1] = 1.0
-        mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
-        std_auc_test = np.std(aucs_test)
-        plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Never-before-seen test set ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
-        std_tpr_test = np.std(tprs_test, axis=0)
-        tprs_upper_test = np.minimum(mean_tpr_test + std_tpr_test, 1)
-        tprs_lower_test = np.maximum(mean_tpr_test - std_tpr_test, 0)
-        plt.fill_between(mean_fpr_test, tprs_lower_test, tprs_upper_test, color='tomato', alpha=.2, label=r'$\pm$ 1 std. dev.')
-        mean_tpr = np.mean(tprs, axis=0)
-        mean_tpr[-1] = 1.0
-        mean_auc = auc(mean_fpr, mean_tpr)
-        std_auc = np.std(aucs)
-        plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean cross-val ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
-        std_tpr = np.std(tprs, axis=0)
-        tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
-        tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
-        plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='dodgerblue', alpha=.2, label=r'$\pm$ 1 std. dev.')
-        plt.xlim([-0.05, 1.05])
-        plt.ylim([-0.05, 1.05])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('ROC for %s' % models)
-        plt.legend(loc="lower right", fontsize=8)
-        save_results_to = 'results/figures/'
-        plt.savefig(save_results_to + str(models) + ".png", format="PNG", dpi=1000)
+    plt.plot([0, 1], [0, 1], linestyle='--', color='green', label='Random', alpha=.8)
+    mean_tpr_test = np.mean(tprs_test, axis=0)
+    mean_tpr_test[-1] = 1.0
+    mean_auc_test = auc(mean_fpr_test, mean_tpr_test)
+    std_auc_test = np.std(aucs_test)
+    plt.plot(mean_fpr_test, mean_tpr_test, color='r', label=r'Never-before-seen test set ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc_test, std_auc_test), lw=2, alpha=.8)
+    std_tpr_test = np.std(tprs_test, axis=0)
+    tprs_upper_test = np.minimum(mean_tpr_test + std_tpr_test, 1)
+    tprs_lower_test = np.maximum(mean_tpr_test - std_tpr_test, 0)
+    plt.fill_between(mean_fpr_test, tprs_lower_test, tprs_upper_test, color='tomato', alpha=.2, label=r'$\pm$ 1 std. dev.')
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    plt.plot(mean_fpr, mean_tpr, color='b', label=r'Mean cross-val ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc), lw=2, alpha=.8)
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    plt.fill_between(mean_fpr, tprs_lower, tprs_upper, color='dodgerblue', alpha=.2, label=r'$\pm$ 1 std. dev.')
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC for %s' % models)
+    plt.legend(loc="lower right", fontsize=8)
+    save_results_to = 'results/figures/'
+    plt.savefig(save_results_to + str(models) + ".png", format="PNG", dpi=1000)
