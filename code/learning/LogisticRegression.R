db9bdfd9e7b61b78e908117ba79a2079c43e61cb
Begum <topcuoglu.begum@gmail.com>
2018-10-04 16:46:44 -0400

    caret addition

--- code/learning/LogisticRegression.R
+++ code/learning/LogisticRegression.R
@@ -1,4 +1,4 @@
-deps = c("pROC","randomForest","AUCRF","knitr","rmarkdown","vegan","gtools", "tidyverse");
+deps = c("caret","knitr","rmarkdown","vegan","gtools", "tidyverse");
 for (dep in deps){
   if (dep %in% installed.packages()[,"Package"] == FALSE){
     install.packages(as.character(dep), quiet=TRUE);
@@ -13,12 +13,41 @@ shared <- read.delim('data/baxter.0.03.subsample.shared', header=T, sep='\t') %>
 
 data <- inner_join(meta, shared, by=c("sample"="Group")) %>% 
   filter(dx != 'adenoma') %>% 
-  mutate(dx = ifelse(dx == "normal",0,1)) %>% 
-  select(-sample)
+  select(-sample) 
   
+data$dx <- factor(data$dx, labels=c("normal", "cancer"))
 
-require(caTools)
-set.seed(101) 
-sample = sample.split(data, SplitRatio = .75)
+
+inTraining <- createDataPartition(data$dx, p = .80, list = FALSE)
+training <- data[ inTraining,] 
+testing  <- data[-inTraining,]
+x_train <- training %>% select(-dx)
+y_train <- ifelse(training$dx == "cancer", 1, 0)
+y_train <- as.factor(y_train)
+fitControl <- trainControl(method = "repeatedcv",number = 5,repeats = 1)
+
+set.seed(825)
+model <- train(x=x_train, y=y_train,method = "glm", trControl = fitControl)
+
+  
+set.seed(20081989) 
+sample = sample.split(data$dx, SplitRatio = .80)
 train = subset(data, sample == TRUE)
 test  = subset(data, sample == FALSE)
+
+
+
+x_train <- model.matrix(dx~., train)[,-1]
+y_train <- ifelse(train$dx == "cancer", 1, 0)
+x_test <- model.matrix(dx~., test)[,-1]
+y_test <- ifelse(test$dx == "cancer", 1, 0)
+## Hyper-parameter tuning
+# prepare training scheme
+# prepare training scheme
+cv.lasso <- cv.glmnet(x_train, y_train, nfolds=5, alpha = 1, family = "binomial", type.measure = "auc")
+model <- glmnet(x_train, y_train, alpha = 1, family = "binomial",
+                lambda = cv.lasso$lambda.min)
+
+
+
+
