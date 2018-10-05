16d6c1c90a7f416676e20a80d489691812da4ae9
Begum <topcuoglu.begum@gmail.com>
2018-10-05 16:55:33 -0400

    save auc for cv and test logit

--- code/learning/LogisticRegression.R
+++ code/learning/LogisticRegression.R
@@ -16,38 +16,34 @@ data <- inner_join(meta, shared, by=c("sample"="Group")) %>%
   select(-sample) 
   
 data$dx <- factor(data$dx, labels=c("normal", "cancer"))
-
-
-inTraining <- createDataPartition(data$dx, p = .80, list = FALSE)
-training <- data[ inTraining,] 
-testing  <- data[-inTraining,]
-x_train <- training %>% select(-dx)
-y_train <- ifelse(training$dx == "cancer", 1, 0)
-y_train <- as.factor(y_train)
-fitControl <- trainControl(method = "repeatedcv",number = 5,repeats = 1)
-
-set.seed(825)
-model <- train(x=x_train, y=y_train,method = "glm", trControl = fitControl, verbose=TRUE, warnings())
-
-  
-set.seed(20081989) 
-sample = sample.split(data$dx, SplitRatio = .80)
-train = subset(data, sample == TRUE)
-test  = subset(data, sample == FALSE)
-
-
-
-x_train <- model.matrix(dx~., train)[,-1]
-y_train <- ifelse(train$dx == "cancer", 1, 0)
-x_test <- model.matrix(dx~., test)[,-1]
-y_test <- ifelse(test$dx == "cancer", 1, 0)
-## Hyper-parameter tuning
-# prepare training scheme
-# prepare training scheme
-cv.lasso <- cv.glmnet(x_train, y_train, nfolds=5, alpha = 1, family = "binomial", type.measure = "auc")
-model <- glmnet(x_train, y_train, alpha = 1, family = "binomial",
-                lambda = cv.lasso$lambda.min)
-
-
+test_auc_list = list()
+cv_auc_list = list()
+for (i in 1:10) {
+  set.seed(20081989)
+  inTraining <- createDataPartition(data$dx, p = .80, list = FALSE)
+  training <- data[ inTraining,] 
+  testing  <- data[-inTraining,]
+  x_train <- training %>% select(-dx)
+  y_train <- training$dx 
+  y_train <- as.factor(y_train)
+  grid <-  expand.grid(alpha=1, lambda = seq(0.01, 0.1, length = 100))
+  cv <- trainControl(method="repeatedcv",repeats = 10, number=5, returnResamp="all",classProbs=TRUE,summaryFunction=twoClassSummary, indexFinal=NULL, savePredictions = TRUE)
+  L2LogicalRegression <- train(x_train, y_train, method = "glmnet", trControl = cv, metric = "ROC",  tuneGrid = grid, family = "binomial")
+  cv_auc <- getTrainPerf(L2LogicalRegression)$TrainROC
+  # best parameter
+  print(L2LogicalRegression$bestTune)
+  # plot parameter performane 
+  #trellis.par.set(caretTheme())
+  #plot(L2LogicalRegression)
+  # Select a parameter setting
+  selectedIndices <- L2LogicalRegression$pred$lambda == L2LogicalRegression$bestTune$lambda
+  rpartProbs <- predict(L2LogicalRegression, testing, type="prob")
+  test_roc <- roc(ifelse(testing$dx == "cancer", 1, 0), rpartProbs[[2]])
+  test_auc <- test_roc$auc
+  cv_auc_list[[i]] <- cv_auc
+  test_auc_list[[i]] <- test_auc
+}
+test_auc_list = do.call(rbind, test_auc_list)
+cv_auc_list = do.call(rbind, cv_auc_list)
 
 
