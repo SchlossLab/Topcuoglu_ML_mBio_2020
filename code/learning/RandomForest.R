e1590493537a7b89bb3555358196c61d02fb7a44
Begum <topcuoglu.begum@gmail.com>
2018-10-12 10:11:56 -0400

    initial commit

---
+++ code/learning/RandomForest.R
@@ -0,0 +1,147 @@
+#### Author: Begum Topcuoglu
+#### Date: 2018-10-11
+#### Title: Logistic Regression Pipeline for Baxter GLNE007 Dataset
+
+#### Description: This script will read in 0.03 subsampled OTU dataset and the metadata that has the cancer diagnosis. It generates a Random Forest model. The model is trained on 80% of the data and then tested on 20% of the data. It also plots the cross validation and testing ROC curves to look at generalization performance of the model.
+
+#### To be able to run this script we need to be in our project directory.
+
+#### The dependinces for this script are consolidated in the first part 
+deps = c("pROC", "caret","knitr","rmarkdown","vegan","gtools", "tidyverse");
+for (dep in deps){
+  if (dep %in% installed.packages()[,"Package"] == FALSE){
+    install.packages(as.character(dep), quiet=TRUE);
+  }
+  library(dep, verbose=FALSE, character.only=TRUE)
+}
+
+# Read in metadata and select only sample Id and diagnosis columns
+meta <- read.delim('data/metadata.tsv', header=T, sep='\t') %>% 
+  select(sample, dx)
+
+# Read in OTU table and remove label and numOtus columns 
+shared <- read.delim('data/baxter.0.03.subsample.shared', header=T, sep='\t') %>% 
+  select(-label, -numOtus)
+
+# Merge metadata and OTU table and remove all the samples that are diagnosed with adenomas. Keep only cancer and normal.
+# Then remove the sample ID column
+data <- inner_join(meta, shared, by=c("sample"="Group")) %>% 
+  filter(dx != 'adenoma') %>% 
+  select(-sample) 
+
+# We want the diagnosis column to a factor
+data$dx <- factor(data$dx, labels=c("normal", "cancer"))
+
+# Create 
+all.test.response <- all.test.predictor <- test_aucs <- c()
+all.cv.response <- all.cv.predictor <- cv_aucs <- c()
+for (i in 1:5) {
+  inTraining <- createDataPartition(data$dx, p = .80, list = FALSE)
+  training <- data[ inTraining,] 
+  testing  <- data[-inTraining,]
+  x_train <- training %>% select(-dx)
+  y_train <- training$dx 
+  y_train <- as.factor(y_train)
+  
+  grid <-  expand.grid(mtry = seq(500, 6920, length = 5))
+                       
+  
+  cv <- trainControl(method="repeatedcv",
+                     repeats = 5, 
+                     number=5, 
+                     returnResamp="final",
+                     classProbs=TRUE,
+                     summaryFunction=twoClassSummary, 
+                     indexFinal=NULL, 
+                     savePredictions = TRUE)
+  
+  L2LogicalRegression <- train(x_train, y_train, 
+                               method = "rf", 
+                               trControl = cv, 
+                               metric = "ROC",  
+                               tuneGrid = grid 
+                               )
+  
+  # Mean AUC value of the best lambda parameter training over repeats
+  cv_auc <- getTrainPerf(L2LogicalRegression)$TrainROC
+  # Best lambda parameter
+  print(L2LogicalRegression$bestTune)
+  # Plot parameter performane
+  #trellis.par.set(caretTheme())
+  #plot(L2LogicalRegression)
+  # Predict on the test set and get predicted probabilities
+  rpartProbs <- predict(L2LogicalRegression, testing, type="prob")
+  # Test AUC calculation
+  test_roc <- roc(ifelse(testing$dx == "cancer", 1, 0), rpartProbs[[2]])
+  test_auc <- test_roc$auc
+  # Save all the test AUCs over iterations in test_aucs
+  test_aucs <- c(test_aucs, test_auc)
+  # Cross-validation mean AUC value
+  cv_auc <- getTrainPerf(L2LogicalRegression)$TrainROC
+  # Save all the test AUCs over iterations in cv_aucs
+  cv_aucs <- c(cv_aucs, cv_auc)
+  # Save the test set labels in all.test.response. Labels converted to 0 for normal and 1 for cancer
+  all.test.response <- c(all.test.response, ifelse(testing$dx == "cancer", 1, 0))
+  # Save the test set predicted probabilities of highest class in all.test.predictor
+  all.test.predictor <- c(all.test.predictor, rpartProbs[[2]])
+  # Save the training set labels in all.test.response. Labels are in the obs column in the training object
+  #all.cv.response <- c(all.cv.response, L2LogicalRegression$pred$obs)
+  # Save the training set labels 
+  #all.cv.predictor <- c(all.cv.predictor, L2LogicalRegression$pred$normal)
+}
+
+# Get the ROC of both test and cv from all the iterations
+test_roc <- roc(all.test.response, all.test.predictor, auc=TRUE, ci=TRUE)
+cv_roc <- roc(all.cv.response, all.cv.predictor, auc=TRUE, ci=TRUE)
+
+par(mar=c(4,4,1,1))
+# Plot random line on ROC curve
+plot(c(1,0),c(0,1), 
+     type='l', 
+     lty=3, 
+     xlim=c(1.01,0), ylim=c(-0.01,1.01), 
+     xaxs='i', yaxs='i', 
+     ylab='', xlab='')
+# Plot Test ROC in red line
+plot(test_roc, 
+     col='black', 
+     lwd=2, 
+     add=T, 
+     lty=1)
+# Plot Test ROC in blue line
+plot(cv_roc, 
+     col='blue', 
+     lwd=2, 
+     add=T, 
+     lty=1)
+# Label the axes
+mtext(side=2, 
+      text="Sensitivity", 
+      line=2.5, 
+      cex=1.5)
+mtext(side=1, 
+      text="Specificity", 
+      line=2.5, 
+      cex=1.5)
+# Add legends for both lines
+legend(x=0.7,y=0.2, 
+       legend=(sprintf('Test - AUC: %.3g', test_roc$auc)), 
+       bty='n', 
+       xjust=0, 
+       lty=c(1,1), 
+       col='black',  
+       text.col='black')
+
+sens.ci <- ci.se(test_roc)
+plot(sens.ci, type="shape", col="gray88")
+
+
+# Save the figure
+dev.copy(png,'RandomForest_inR.png', 
+         width = 500, 
+         height = 500,  
+         res=96)
+dev.off()
+
+
+
