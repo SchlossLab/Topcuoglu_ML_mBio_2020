5bcaa42e330d93b41106622585545c0b4c559bea
Begum <topcuoglu.begum@gmail.com>
2018-10-23 12:19:14 -0400

    change max_features based on Yanqisemail

--- code/learning/model_selection.py
+++ code/learning/model_selection.py
@@ -48,7 +48,7 @@ def select_model(net):
         param_grid = dict(C=c_values, gamma=gamma)
     if net=="Random_Forest":
         model = RandomForestClassifier(bootstrap= True)
-        n_estimators = [500, 1000, 1500]
-        max_features= [500, 1000, 1500]
+        n_estimators = [1000]
+        max_features= [10, 80, 500, 1000, 1500]
         param_grid = dict(n_estimators=n_estimators, max_features=max_features)
     return model, param_grid, cv
