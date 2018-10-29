9e4fd3e9a1477aef094fceff377c58eb3aae0536
Begum <topcuoglu.begum@gmail.com>
2018-10-29 13:28:27 -0400

    change commented description

--- code/learning/main.py
+++ code/learning/main.py
@@ -3,7 +3,7 @@
 # Author: Begum Topcuoglu
 # Date: 2018-09-26
 #
-# This script runs Logistic Regression analysis on Baxter Dataset subset of onlt cancer and normal samples to predict diagnosis based on OTU data only. This script only evaluates generalization performance of the model.
+# This script runs all the models on Baxter Dataset subset of onlt cancer and normal samples to predict diagnosis based on OTU data only. This script only evaluates generalization performance of the model.
 #
 
 ############## IMPORT MODULES ######################
