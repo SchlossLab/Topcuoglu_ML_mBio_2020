d3ca7f12f0d6ab5061078585fb792d10f3f5fabf
Begum <topcuoglu.begum@gmail.com>
2018-10-30 15:53:47 -0400

    color changes

--- code/learning/compareAUC.R
+++ code/learning/compareAUC.R
@@ -6,30 +6,40 @@ for (dep in deps){
   library(dep, verbose=FALSE, character.only=TRUE)
 }
 
-# Read in metadata and select only sample Id and diagnosis columns
+
 logit <- read.delim('data/process/L2_Logistic_Regression.tsv', header=T, sep='\t') %>%
   select(level_1, AUC) %>% 
-  rename(Performance = level_1)
+  rename(Performance = level_1) %>% 
+  mutate(model="L2-Logistic Regression")
 
 logit$Performance <- factor(logit$Performance,
                            labels = c("Cross-validation", "Testing"))
 
+l1svm <- read.delim('data/process/L1_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>% 
+  rename(Performance = level_1) %>% 
+  mutate(model="L1-SVM Linear Kernel") %>% 
+  bind_rows(logit) %>% 
+  group_by(model)
 
-ggplot(logit, aes(x = Performance, y = AUC, fill = Performance)) +
+
+
+ggplot(l1svm, aes(x = model, y = AUC, fill = Performance)) +
   geom_boxplot(alpha=0.7) +
   scale_y_continuous(name = "AUC",
-                     breaks = seq(0, 1, 0.2),
-                     limits=c(0, 1)) +
+                     breaks = seq(0.4, 1, 0.05),
+                     limits=c(0.4, 1)) +
   scale_x_discrete(name = "") +
   theme_bw() +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
-    plot.title = element_text(size = 14, family = "Tahoma", face = "bold"),
-        text = element_text(size = 12, family = "Tahoma"),
-        axis.title = element_text(face="bold"),
-        axis.text.x=element_text(size = 11)) +
-  scale_fill_brewer(palette = "Accent")
+        text = element_text(size = 12),
+        axis.text.x=element_text(size = 15, colour='black'),
+        axis.text.y=element_text(size = 12, colour='black'), 
+        axis.title.y=element_text(size = 20), 
+        axis.title.x=element_text(size = 20)) +
+  scale_fill_brewer(palette = "Paired")
 
 
   
