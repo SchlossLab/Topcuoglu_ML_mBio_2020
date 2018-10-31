bcfc6c930336aba4530b3c14d435d0b22ef581fc
BegumTop <begumtop@umich.edu>
2018-10-31 08:49:12 -0400

    added svm rbf

--- code/learning/compareAUC.R
+++ code/learning/compareAUC.R
@@ -24,29 +24,35 @@ l1svm <- read.delim('data/process/L1_SVM_Linear_Kernel.tsv', header=T, sep='\t')
 l2svm <- read.delim('data/process/L2_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
   select(level_1, AUC) %>% 
   rename(Performance = level_1) %>% 
-  mutate(model="L2-SVM Linear Kernel")%>% 
-  bind_rows(logit, l1svm) %>% 
+  mutate(model="L2-SVM Linear Kernel")
+
+
+svmRBF <- read.delim('data/process/SVM_RBF.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>% 
+  rename(Performance = level_1) %>% 
+  mutate(model="SVM RBF")%>% 
+  bind_rows(logit, l1svm, l2svm) %>% 
   group_by(model)
 
 
 
-ggplot(l2svm, aes(x = model, y = AUC, fill = Performance)) +
+ggplot(svmRBF, aes(x = model, y = AUC, fill = Performance)) +
   geom_boxplot(alpha=0.7) +
   scale_y_continuous(name = "AUC",
-                     breaks = seq(0.4, 1, 0.05),
-                     limits=c(0.4, 1)) +
+                     breaks = seq(0.3, 1, 0.05),
+                     limits=c(0.3, 1)) +
   scale_x_discrete(name = "") +
   theme_bw() +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         text = element_text(size = 12),
-        axis.text.x=element_text(size = 15, colour='black'),
+        axis.text.x=element_text(size = 12, colour='black'),
         axis.text.y=element_text(size = 12, colour='black'), 
         axis.title.y=element_text(size = 20), 
         axis.title.x=element_text(size = 20)) +
   scale_fill_brewer(palette = "Paired")
 
-ggsave("AUC_comparison.pdf", plot = last_plot(), device = 'pdf', path = 'results/figures', width = 10, height = 10)
+ggsave("AUC_comparison.pdf", plot = last_plot(), device = 'pdf', path = 'results/figures', width = 12, height = 10)
 
   
