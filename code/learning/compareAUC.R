4147a657df2c6884d623b84ebbce7df7f95c549b
BegumTop <begumtop@umich.edu>
2018-11-09 16:31:26 -0500

    path fix for make

--- code/learning/compareAUC.R
+++ code/learning/compareAUC.R
@@ -2,8 +2,8 @@
 # Date: 2018-11-06
 #
 ######################################################################
-# 1. This script loads the .tsv files generated from python code main.py for all the models. The .tsv files have AUC values for cross validation (for each repeat and n-fold) and for testing for each iteration over different splits of the training-testing data. 
-# 2. This script then binds all the models together and makes a boxplot for AUC for all the models. 
+# 1. This script loads the .tsv files generated from python code main.py for all the models. The .tsv files have AUC values for cross validation (for each repeat and n-fold) and for testing for each iteration over different splits of the training-testing data.
+# 2. This script then binds all the models together and makes a boxplot for AUC for all the models.
 ######################################################################
 
 
@@ -26,70 +26,70 @@ for (dep in deps){
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is MinMax scaler
-logit <- read.delim('data/process/L2_Logistic_Regression.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+logit <- read.delim('../../data/process/L2_Logistic_Regression.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="L2-Logistic Regression")
 
 # Read in AUCs table generated from l1 SVM linear kernel for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is Standard scaler
-l1svm <- read.delim('data/process/L1_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
-  mutate(model="L1-SVM Linear") 
+l1svm <- read.delim('../..data/process/L1_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
+  mutate(model="L1-SVM Linear")
 
 # Read in AUCs table generated from l2 SVM linear kernel for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is Standard scaler
-l2svm <- read.delim('data/process/L2_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+l2svm <- read.delim('../../data/process/L2_SVM_Linear_Kernel.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="L2-SVM Linear")
 
 # Read in AUCs table generated from  SVM RBF kernel for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is Standard scaler
-svmRBF <- read.delim('data/process/SVM_RBF.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+svmRBF <- read.delim('../../data/process/SVM_RBF.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="SVM RBF")
 
 # Read in AUCs table generated from xgboost for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is MinMax (0-1) scaler
-xgboost <- read.delim('data/process/XGBoost.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+xgboost <- read.delim('../../data/process/XGBoost.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="XGBoost")
 
 # Read in AUCs table generated from random forest for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is MinMax (0-1) scaler
-rf <- read.delim('data/process/Random_Forest.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+rf <- read.delim('../../data/process/Random_Forest.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="Random Forest ")
 
 # Read in AUCs table generated from decision tree for all samples
 #       Carcinomas + Adenomas are 1 and Normal is 0 for binary.
 #       FIT is a feature
 #       The scaler is MinMax (0-1) scaler
-dt <- read.delim('data/process/Decision_Tree.tsv', header=T, sep='\t') %>%
-  select(level_1, AUC) %>% 
-  rename(Performance = level_1) %>% 
+dt <- read.delim('../../data/process/Decision_Tree.tsv', header=T, sep='\t') %>%
+  select(level_1, AUC) %>%
+  rename(Performance = level_1) %>%
   mutate(model="Decision Tree")
 
 ######################################################################
 #------------ Put all the loaded AUC tables together -----------------#
 ######################################################################
 
-all <- bind_rows(logit, l1svm, l2svm, svmRBF, xgboost, rf, dt) %>% 
+all <- bind_rows(logit, l1svm, l2svm, svmRBF, xgboost, rf, dt) %>%
   group_by(model)
 
 ######################################################################
@@ -105,13 +105,13 @@ ggplot(all, aes(x = fct_reorder(model, AUC, fun = median, .asc =TRUE), y = AUC, 
   theme_bw() +
   theme(legend.text=element_text(size=18),
         legend.title=element_text(size=22),
-        panel.grid.major = element_blank(), 
+        panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         text = element_text(size = 12),
         axis.text.x=element_text(size = 12, colour='black'),
-        axis.text.y=element_text(size = 12, colour='black'), 
-        axis.title.y=element_text(size = 20), 
+        axis.text.y=element_text(size = 12, colour='black'),
+        axis.title.y=element_text(size = 20),
         axis.title.x=element_text(size = 20)) +
   scale_fill_brewer(palette = "Paired") +
   geom_hline(yintercept = 0.5, linetype="dashed") +
@@ -123,5 +123,3 @@ ggplot(all, aes(x = fct_reorder(model, AUC, fun = median, .asc =TRUE), y = AUC, 
 ######################################################################
 
 ggsave("AUC_comparison.pdf", plot = last_plot(), device = 'pdf', path = 'results/figures', width = 15, height = 10)
-
-  
