1f9e282af6763204e292bc1593672f46e9b78ec9
Begum <topcuoglu.begum@gmail.com>
2018-11-16 11:42:03 -0500

    create a SRN classification

--- code/learning/preprocess_data.py
+++ code/learning/preprocess_data.py
@@ -53,3 +53,23 @@ def process_multidata(shared, meta):
     y= y.values
     x = x.dropna()
     return x, y
+
+def process_SRNdata(shared, meta):
+    ## Remove unnecessary columns from meta and only keep label for classification(diagnosis), FIT result and the sample name
+    meta = meta[['sample','Dx_Bin', 'fit_result']]
+    ## Rename the column name "Group" to match the "sample" in meta
+    shared = shared.rename(index=str, columns={"Group":"sample"})
+    ## Merge the 2 datasets on sample
+    data=pd.merge(meta,shared,on=['sample'])
+    ## Drop all except OTU columns for x
+    x = data.drop(["sample", 'Dx_Bin', "numOtus", "label"], axis=1)
+    ## Cancer+adenoma =1 Normal =0
+    diagnosis = {"Adenoma":0, "adv Adenoma":1, "Cancer":1, "Normal":0, "High Risk Normal":0}
+    ## Generate y which had diagnosis 0, 1, 2
+    y = data['Dx_Bin'].replace(diagnosis)
+    # y = np.eye(2, dtype='uint8')[y]
+    ## Drop if NA elements
+    y = y.dropna()
+    y= y.values
+    x = x.dropna()
+    return x, y
