0a8870a44263583e7d94e58bee19979d375b44e6
Begum <topcuoglu.begum@gmail.com>
2018-12-03 11:08:27 -0500

    seed to all model selection

--- code/learning/model_selection.py
+++ code/learning/model_selection.py
@@ -29,30 +29,30 @@ def select_model(net):
     ## With the below if statements, we will define which classifier will be used.
     ## We will also define the hyper-parameters to be tuned and their budget for each model.
     if net=="L2_Logistic_Regression":
-        model = linear_model.LogisticRegression()
+        model = linear_model.LogisticRegression(random_state=200889)
         c_values = [0.01, 0.1, 0.25, 0.5, 0.8, 0.9, 1, 10]
         param_grid = dict(C=c_values)
     if net=="L1_SVM_Linear_Kernel":
-        model= LinearSVC(penalty='l1', loss='squared_hinge', dual=False)
+        model= LinearSVC(penalty='l1', loss='squared_hinge', dual=False, random_state=200889)
         c_values = [0.00001, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1 ,1]
         param_grid = dict(C=c_values)
     if net=="L2_SVM_Linear_Kernel":
-        model= LinearSVC(penalty='l2', loss='squared_hinge', dual=False)
+        model= LinearSVC(penalty='l2', loss='squared_hinge', dual=False, random_state=200889)
         c_values = [0.00001, 0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1 ,1]
         param_grid = dict(C=c_values)
     if net=="SVM_RBF":
-        model = SVC(kernel='rbf')
+        model = SVC(kernel='rbf', random_state=200889)
         c_values = [0.00001, 0.0001, 0.001, 0.01]
         gamma = [0.000000000001, 0.00000000001, 0.0000000001, 0.000000001]
         param_grid = dict(C=c_values, gamma=gamma)
     if net=="Random_Forest":
         cv = RepeatedStratifiedKFold(n_splits=5, n_repeats=25, random_state=200889)
-        model = RandomForestClassifier(bootstrap= True)
+        model = RandomForestClassifier(bootstrap= True, random_state=200889)
         n_estimators = [1000]
         max_features= [10, 80, 500, 1000, 1500, 2000, 3000]
         param_grid = dict(n_estimators=n_estimators, max_features=max_features)
     if net=="Decision_Tree":
-        model = DecisionTreeClassifier()
+        model = DecisionTreeClassifier(random_state=200889)
         max_depth=[6, 8, 10, 50]
         min_samples_split=[10, 25, 50]
         param_grid = dict(max_depth=max_depth, min_samples_split=min_samples_split)
@@ -64,7 +64,7 @@ def select_model(net):
         'colsample_bytree': 0.8,
         'objective': 'binary:logistic'
         }
-        model = xgb.XGBClassifier(**ind_params)
+        model = xgb.XGBClassifier(**ind_params, random_state=200889)
         param_grid = {
         'n_estimators':[100],
         'learning_rate':[0.01, 0.1, 1],
@@ -78,7 +78,7 @@ def select_model(net):
 
 
 ## Print out the parameters that are being optimized for each model as a dataframe and export as .csv.
-## This file will be used to generate Table 2 in our manuscript. 
+## This file will be used to generate Table 2 in our manuscript.
 models = ["L2_Logistic_Regression", "L1_SVM_Linear_Kernel", "L2_SVM_Linear_Kernel", "SVM_RBF", "Random_Forest", "Decision_Tree", "XGBoost"]
 params = []
 for models in models:
