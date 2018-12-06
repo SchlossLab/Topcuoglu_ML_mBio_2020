ef7889eba09c8e1bfbe84dd29863328dc2d4def3
Begum <topcuoglu.begum@gmail.com>
2018-12-06 09:36:40 -0500

    DRY plotting

--- code/learning/individual_models/L2LinearSVM_plots.R
+++ code/learning/individual_models/L2LinearSVM_plots.R
@@ -9,6 +9,8 @@ for (dep in deps){
   library(dep, verbose=FALSE, character.only=TRUE)
 }
 
+# Load in needed functions and libraries
+source('code/learning/functions.R')
 
 # Read in hyper-parameter AUCs generated from L2 logistic regression model for all samples:
 l2svm <- read.delim('data/process/L2_Linear_SVM_aucs_hps_R.tsv', header=T, sep='\t')
@@ -19,58 +21,12 @@ l2svm %>%
             cv_sd=sd(cv_aucs), 
             test_sd=sd(test_aucs))
 
-l2svm_melt <- l2svm %>%
-  mutate(model="L2 Linear SVM") %>% 
-  melt(measure.vars=c('cv_aucs', 'test_aucs')) %>% 
-  rename(AUC=value) %>% 
-  rename(Performance=variable) %>% 
-  group_by(Performance)
-
-
-performance <- ggplot(l2svm_melt, aes(x = model, 
-                                      y = AUC, 
-                                      fill = Performance)) +
-  geom_boxplot(alpha=0.7) +
-  scale_y_continuous(name = "AUC",
-                     breaks = seq(0.5, 1, 0.02),
-                     limits=c(0.5, 1), expand=c(0,0)) +
-  scale_x_discrete(name = "") +
-  theme_bw() +
-  theme(legend.text=element_text(size=8),
-        legend.title=element_text(size=10),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        text = element_text(size = 12),
-        axis.text.x=element_text(size = 15, colour='black'),
-        axis.text.y=element_text(size = 10, colour='black'),
-        axis.title.y=element_text(size = 15),
-        axis.title.x=element_text(size = 15)) +
-  scale_fill_brewer(palette = "Paired") +
-  geom_hline(yintercept = 0.5, linetype="dashed")
-
-
-
-parameter <- ggplot(l2svm, aes(x = Cost, y = cv_aucs))+
-  geom_point()+
-  stat_summary(aes(y = cv_aucs,group=1), fun.y=mean, colour="black", geom="line",group=1) + 
-  theme_bw() +
-  theme(legend.text=element_text(size=18),
-        legend.title=element_text(size=22),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        panel.background = element_blank(),
-        text = element_text(size = 12),
-        axis.text.x=element_text(size = 10, colour='black'),
-        axis.text.y=element_text(size = 10, colour='black'),
-        axis.title.y=element_text(size = 15),
-        axis.title.x=element_text(size = 15)) +
-  scale_y_continuous(name = "Cross-validation AUC",
-                     breaks = seq(0.5, 1, 0.02),
-                     limits=c(0.5, 1), expand=c(0,0))
-
+performance <- plot_performance(l2svm)
+parameter <- plot_parameter(l2svm)
 
 plot_grid(performance, parameter, labels = c("A", "B"))
+
+
 
 ######################################################################
 #-----------------------Save figure as .pdf ------------------------ #
