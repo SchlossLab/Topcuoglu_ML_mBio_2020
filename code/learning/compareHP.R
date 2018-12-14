905e87de48c71e63d0cb4d29ad9d423fb93fda86
Begum <topcuoglu.begum@gmail.com>
2018-12-14 17:02:53 -0500

    all the HP from python

--- code/learning/compareHP.R
+++ code/learning/compareHP.R
@@ -31,8 +31,8 @@ logit_plot <- ggplot(logit,aes(x=C,y=meanAUC)) +
   geom_line() +
   geom_point() +
   scale_y_continuous(name="Logistic Regression cvAUC",
-                     limits = c(0.50, 0.80),
-                     breaks = seq(0.5, 0.8, 0.05)) +
+                     limits = c(0.50, 1),
+                     breaks = seq(0.5, 1, 0.05)) +
   scale_x_continuous(name="C (penalty)") +
   theme_bw() +
   theme(legend.text=element_text(size=18),
@@ -56,8 +56,8 @@ l1svm_plot <- ggplot(l1svm,aes(x=C,y=meanAUC)) +
   geom_line() +
   geom_point() +
   scale_y_continuous(name="L1 Linear SVM cvAUC",
-                     limits = c(0.50, 0.80),
-                     breaks = seq(0.5, 0.8, 0.05)) +
+                     limits = c(0.50, 1),
+                     breaks = seq(0.5, 1, 0.05)) +
   scale_x_continuous(name="C (penalty)") +
   theme_bw() +
   theme(legend.text=element_text(size=18),
@@ -83,8 +83,8 @@ l2svm_plot <- ggplot(l2svm,aes(x=C,y=meanAUC)) +
   geom_line() +
   geom_point() +
   scale_y_continuous(name="L2 Linear SVM cvAUC",
-                     limits = c(0.50, 0.80),
-                     breaks = seq(0.5, 0.8, 0.05)) +
+                     limits = c(0.50, 1),
+                     breaks = seq(0.5, 1, 0.05)) +
   scale_x_continuous(name="C (penalty)") +
   theme_bw() +
   theme(legend.text=element_text(size=18),
@@ -102,16 +102,45 @@ l2svm_plot <- ggplot(l2svm,aes(x=C,y=meanAUC)) +
 
 svmRBF <- read.delim('data/process/SVM_RBF_parameters.tsv', header=T, sep='\t')%>%
   select(-X) %>%
-  mutate(meanAUC=rowMeans(.[, 3:101])) %>%  # Take the mean of the rows for each C
-  select(C, gamma, meanAUC)
+  mutate(meanAUC=rowMeans(.[, 3:102])) %>%  # Take the mean of the rows for each C
+  select(C, gamma, meanAUC) %>% 
+  group_by(gamma) 
 
-svmRBF_plot_C <- ggplot(svmRBF,aes(x=C,y=meanAUC)) +
+svmRBF_plot_C_sigma <- ggplot(svmRBF,aes(x=C,y=meanAUC))+
+  geom_point() +
+  facet_grid(~gamma, labeller = labeller()) +
+  geom_line() +
+  scale_y_continuous(name="RBF SVM cvAUC",
+                     limits = c(.70, .71),
+                     breaks = seq(.70, .71, 0.001)) +
+  scale_x_log10(labels = scales::trans_format("log10", scales::math_format(10^.x))) +
+  theme_bw() +
+  theme(panel.spacing = unit(1, "lines"),
+        legend.text=element_text(size=18),
+        legend.title=element_text(size=22),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        text = element_text(size = 12),
+        axis.text.x=element_text(size = 12, colour='black'),
+        axis.text.y=element_text(size = 12, colour='black'),
+        axis.title.y=element_text(size = 13),
+        axis.title.x=element_text(size = 13))
+
+
+rf <- read.delim('data/process/Random_Forest_parameters.tsv', header=T, sep='\t')%>%
+  select(-X) %>%
+  mutate(meanAUC=rowMeans(.[, 3:102])) %>% # Take the mean of the rows for each C
+  select(max_features, meanAUC)
+  
+
+rf_plot_max <- ggplot(rf,aes(x=max_features,y=meanAUC)) +
   geom_line() +
   geom_point() +
-  scale_y_continuous(name="RBF SVM cvAUC",
-                     limits = c(0.50, 0.80),
-                     breaks = seq(0.5, 0.8, 0.05)) +
-  scale_x_continuous(name="C (penalty)") +
+  scale_y_continuous(name="Random Forest cvAUC",
+                     limits = c(0.50, 1),
+                     breaks = seq(0.5, 1, 0.05)) +
+  scale_x_continuous(name="max features") +
   theme_bw() +
   theme(legend.text=element_text(size=18),
         legend.title=element_text(size=22),
@@ -124,13 +153,20 @@ svmRBF_plot_C <- ggplot(svmRBF,aes(x=C,y=meanAUC)) +
         axis.title.y=element_text(size = 13),
         axis.title.x=element_text(size = 13))
 
-svmRBF_plot_gamma <- ggplot(svmRBF,aes(x=gamma,y=meanAUC)) +
-  geom_line() +
+dt <- read.delim('data/process/Decision_Tree_parameters.tsv', header=T, sep='\t')%>%
+  select(-X) %>%
+  mutate(meanAUC=rowMeans(.[, 3:102])) %>% # Take the mean of the rows for each C
+  select(max_depth, min_samples_split, meanAUC) %>% 
+  group_by(max_depth)
+
+dt_plot_max <- ggplot(dt,aes(x=min_samples_split,y=meanAUC)) +
   geom_point() +
-  scale_y_continuous(name="RBF SVM cvAUC",
-                     limits = c(0.50, 0.80),
-                     breaks = seq(0.5, 0.8, 0.05)) +
-  scale_x_continuous(name="gamma") +
+  facet_grid(~max_depth) +
+  geom_line() +
+  scale_y_continuous(name="Decision Tree cvAUC",
+                     limits = c(0.65, 0.75),
+                     breaks = seq(0.65, 0.75, 0.01)) +
+  scale_x_continuous(name="max depth") +
   theme_bw() +
   theme(legend.text=element_text(size=18),
         legend.title=element_text(size=22),
@@ -143,7 +179,8 @@ svmRBF_plot_gamma <- ggplot(svmRBF,aes(x=gamma,y=meanAUC)) +
         axis.title.y=element_text(size = 13),
         axis.title.x=element_text(size = 13))
 
-plot_grid(logit_plot, l1svm_plot, l2svm_plot, svmRBF_plot_C, svmRBF_plot_gamma, labels = c("A", "B", "C", "D", "E"))
+
+all <- plot_grid(logit_plot, l1svm_plot, l2svm_plot, svmRBF_plot_C_sigma, rf_plot_max, dt_plot_max, labels = c("A", "B", "C", "D", "E", "F"))
 
 
 
@@ -153,4 +190,5 @@ plot_grid(logit_plot, l1svm_plot, l2svm_plot, svmRBF_plot_C, svmRBF_plot_gamma, 
 #-----------------------Save figure as .pdf ------------------------ #
 ######################################################################
 
-ggsave("HP_comparison.pdf", plot = last_plot(), device = 'pdf', path = 'results/figures', width = 15, height = 10)
+ggsave("HP_comparison.pdf", plot = all, device = 'pdf', path = 'results/figures', width = 20, height = 15)
+
