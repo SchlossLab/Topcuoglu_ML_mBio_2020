cf23d052a37cdfaebdccf7bfa538141e8c9a1f48
Begum <topcuoglu.begum@gmail.com>
2018-12-19 14:31:40 -0500

    cv should be 5fold

--- code/learning/individual_models/DecisionTree.R
+++ code/learning/individual_models/DecisionTree.R
@@ -55,7 +55,7 @@ for (i in 1:100) {
   grid <-  expand.grid(maxdepth = c(1,2,3,4,5,6,7,8,9,10))
   cv <- trainControl(method="repeatedcv",
                      repeats = 10,
-                     number=10,
+                     number=5,
                      returnResamp="final",
                      classProbs=TRUE,
                      summaryFunction=twoClassSummary,
