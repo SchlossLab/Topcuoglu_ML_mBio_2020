63c9547d1262642a8c75e4828310e8b1f8aeb531
BegumTop <begumtop@umich.edu>
2019-01-28 11:37:28 -0500

    run to concatanate

--- code/cat_csv_files.sh
+++ code/cat_csv_files.sh
@@ -3,7 +3,7 @@ SEARCH_DIR=data/temp
 FINAL_DIR=data/process
 # Keep the first line of File1 and remove the first line of all the others and combine
 
-for model in "L2_Logistic_Regression" "L2_Linear_SVM" "RBF_SVM" "Decision_Tree" "Random_Forest" "XGBoost"
+for model in "L2_Linear_SVM" "Random_Forest" "XGBoost"
 do
 	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_hp_results_"$model".csv
 
