566fcc78dbafdc158d22b2112145130fa84bdd57
Begum <topcuoglu.begum@gmail.com>
2019-01-28 11:48:51 -0500

    learning to cat things

--- code/learning/cat_parameter_search.sh
+++ code/learning/cat_parameter_search.sh
@@ -6,4 +6,6 @@ do
 	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_hp_results_"$model".csv
 
 	head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/best_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_best_hp_results_"$model".csv
+
+	
 done
