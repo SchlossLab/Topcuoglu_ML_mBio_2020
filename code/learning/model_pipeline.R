12e9bd2ce21cb88a240ca9ba414cc8fbfea4a36e
Begum <topcuoglu.begum@gmail.com>
2019-02-11 12:45:21 -0500

    delete caret models part

--- code/learning/model_pipeline.R
+++ code/learning/model_pipeline.R
@@ -33,21 +33,6 @@
 #------------------------- DEFINE FUNCTION -------------------#
 ######################################################################
 pipeline <- function(dataset, model){
-  # Download all models file from caret package
-  # We will parse, create and save models.RData here:
-  # ls Library/Frameworks/R.framework/Versions/3.5/Resources/library/caret/models/
-  setwd("data/caret_models")
-  modelFiles <- list.files(pattern = "\\.R$")
-  models <- vector(mode = "list", length = length(modelFiles))
-  names(models) <- gsub("\\.R$", "", modelFiles)
-  for(i in seq(along = modelFiles)) {
-    source(modelFiles[i])
-    models[[i]] <- modelInfo
-    rm(modelInfo)
-  }
-  # Save to your caret package directory, into the models/ subdirectory
-  save(models, file = "~/R/x86_64-pc-linux-gnu-library/3.5/caret/models/models.RData")
-  setwd("/nfs/turbo/schloss-lab/begumtop/DeepLearning")
   # Create vectors to save cv and test AUC values for every data-split
   results_total <-  data.frame()
   test_aucs <- c()
