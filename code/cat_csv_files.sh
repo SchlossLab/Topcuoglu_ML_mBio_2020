963e7a0157b0905fa1d9ee52c1025888b7b9e85f
BegumTop <begumtop@umich.edu>
2019-02-11 12:50:45 -0500

    xgboost

--- code/cat_csv_files.sh
+++ code/cat_csv_files.sh
@@ -3,7 +3,7 @@ SEARCH_DIR=data/temp
 FINAL_DIR=data/process
 # Keep the first line of File1 and remove the first line of all the others and combine
 
-for model in "L2_Linear_SVM" "Random_Forest" "XGBoost"
+for model in "Random_Forest" "XGBoost"
 do
 	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_hp_results_"$model".csv
 
@@ -13,5 +13,5 @@ do
 	mv $SEARCH_DIR/combined_best_hp_results_"$model".csv $FINAL_DIR/combined_best_hp_results_"$model".csv 
 done
 
-rm $SEARCH_DIR/all_hp_*
-rm $SEARCH_DIR/best_hp_*
+#rm $SEARCH_DIR/all_hp_*
+#rm $SEARCH_DIR/best_hp_*
