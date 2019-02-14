4fe7aba137db3dd379c5d7a98d6e77757e5b43f2
Begum <topcuoglu.begum@gmail.com>
2019-02-14 13:51:28 -0500

    fix the cropt up lines

--- code/cat_csv_files.sh
+++ code/cat_csv_files.sh
@@ -5,15 +5,12 @@ FINAL_DIR=data/process
 
 for model in "XGBoost" "Random_Forest" "RBF_SVM"  "L1_Linear_SVM" "L2_Logistic_Regression" "L2_Linear_SVM" "Decision_Tree"
 do
-  	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 $
+    head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_hp_results_"$model".csv
 
-        head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +$
+    head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/best_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_best_hp_results_"$model".csv
 
-        head -1 $SEARCH_DIR/all_imp_features_results_"$model"_1.csv > $SEARCH_DIR/combined_all_imp_features_results_"$mode$
-        mv $SEARCH_DIR/combined_all_hp_results_"$model".csv $FINAL_DIR/combined_all_hp_results_"$model".csv
-        mv $SEARCH_DIR/combined_best_hp_results_"$model".csv $FINAL_DIR/combined_best_hp_results_"$model".csv
-        mv $SEARCH_DIR/combined_all_imp_features_results_"$model".csv $FINAL_DIR/combined_all_imp_features_results_"$model$
+    head -1 $SEARCH_DIR/all_imp_features_results_"$model"_1.csv > $SEARCH_DIR/combined_all_imp_features_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_imp_features_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_imp_features_results_"$model".csv
+    mv $SEARCH_DIR/combined_all_hp_results_"$model".csv $FINAL_DIR/combined_all_hp_results_"$model".csv
+    mv $SEARCH_DIR/combined_best_hp_results_"$model".csv $FINAL_DIR/combined_best_hp_results_"$model".csv
+    mv $SEARCH_DIR/combined_all_imp_features_results_"$model".csv $FINAL_DIR/combined_all_imp_features_results_"$model".csv
 done
-
-#rm $SEARCH_DIR/all_hp_*
-#rm $SEARCH_DIR/best_hp_*
