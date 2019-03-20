ad52a4c2fbb07718caf5a59c57ce04777e134bcd
Begum <topcuoglu.begum@gmail.com>
2019-03-20 11:01:26 -0400

    make xgboost range larger to show drops

--- code/learning/model_selection.R
+++ code/learning/model_selection.R
@@ -159,11 +159,11 @@ tuning_grid <- function(train_data, model){
   else if (model=="XGBoost"){
     grid <-  expand.grid(nrounds=500,
                          gamma=0,
-                         eta=c(0.001, 0.005, 0.01, 0.015, 0.02),
+                         eta=c(0.001, 0.01, 0.1, 1),
                          max_depth=8,
                          colsample_bytree= 0.8,
                          min_child_weight=1,
-                         subsample=c(0.4, 0.5, 0.6))
+                         subsample=c(0.4, 0.5, 0.6, 0.7))
     method <- "xgbTree"
   }
   else {
