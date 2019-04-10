42a61647a0d1a320b54d0825e3a173dd8d11f5c5
Begum <topcuoglu.begum@gmail.com>
2019-04-10 15:48:30 -0400

    change names of figure code

--- code/learning/Figure2.R
+++ code/learning/Figure2.R
@@ -2,173 +2,80 @@
 # Date: 2018-02-12
 #
 ######################################################################
-# This script plots Figure 2:
-#   1. Y axis: mean cvAUC of 100 datasplits
-#   2. X axis: different hyper-parameters tested in cv(hp)
+# This script plots Figure 1:
+#   1. cvAUC (means of 100 repeats for the best hp) of 100 datasplits
+#   2. testAUC of 100 datasplits
 ######################################################################
-
 
 ######################################################################
 # Load in needed functions and libraries
 source('code/learning/functions.R')
 # detach("package:randomForest", unload=TRUE) to run
 ######################################################################
-#----------------- Read in necessary libraries -------------------#
-######################################################################
-deps = c("cowplot", "ggplot2","knitr","rmarkdown","vegan","gtools", "tidyverse");
-for (dep in deps){
-  if (dep %in% installed.packages()[,"Package"] == FALSE){
-    install.packages(as.character(dep), quiet=TRUE);
-  }
-  library(dep, verbose=FALSE, character.only=TRUE)
-}
-######################################################################
 
 
 ######################################################################
-# Load .tsv data generated with modeling pipeline for Logistic Regression
+# Load .csv data generated with modeling pipeline 
 ######################################################################
 
-# Read in the results of trained model of 100 data-splits
+# Read in the cvAUCs, testAUCs for 100 splits.
+best_files <- list.files(path= 'data/process', pattern='combined_best.*', full.names = TRUE)
 
-all_files <- list.files(path= 'data/process', pattern='combined_all.*', full.names = TRUE)
 
-logit_all <- read_files(all_files[4])
-l2svm_all <- read_files(all_files[3])
-l1svm_all <- read_files(all_files[2])
-rbf_all <- read_files(all_files[6])
-rf_all <- read_files(all_files[5])
-dt_all <- read_files(all_files[1])
-xgboost_all <- read_files(all_files[7])
+logit <- read_files(best_files[4])
+l2svm <- read_files(best_files[3])
+l1svm <- read_files(best_files[2])
+rbf <- read_files(best_files[6])
+rf <- read_files(best_files[5])
+dt <- read_files(best_files[1])
+xgboost <- read_files(best_files[7])
+
+
+
+best_performance <- bind_rows(logit, l2svm, rbf, dt, xgboost, rf, l1svm)%>%
+  melt_data()
 
 ######################################################################
-#Plot the mean AUC values for hyper parameters tested #
+#Plot the AUC values for cross validation and testing for each model #
 ######################################################################
 
-# Define the base plot for all the modeling methods
-base_plot <-  function(data, x_axis, y_axis){
-  plot <- ggplot(data, aes(x_axis, y_axis)) +
-  geom_line() +
-  geom_point() +
+
+performance <- ggplot(best_performance, aes(x = fct_reorder(model, AUC), y = AUC, fill = Performance)) +
+  geom_boxplot(alpha=0.7) +
+  scale_fill_manual(values=c("#999999", "#E69F00")) +
+  scale_y_continuous(name = "AUROC",
+                     breaks = seq(0.5, 1, 0.02),
+                     limits=c(0.5, 1), 
+                     expand=c(0,0)) +
+  scale_x_discrete(name = "") +
+  #                 labels=c("L2 Linear SVM", 
+   #                         "RBF SVM",
+    #                        "L2 Logistic Regression",
+     #                       "Decision Tree",
+      #                      "L1 Linear SVM",
+       #                     "XGBoost",
+        #                    "Random Forest")) +
   theme_bw() +
-  theme(legend.text=element_text(size=18),
-        legend.title=element_text(size=22),
+  theme(legend.title = element_blank(),
+        legend.background = element_rect(size=0.5, linetype="solid", color="black"),
+        legend.justification=c(0,1), 
+        legend.position=c(0,1),
+        legend.box.margin=margin(c(10,10,10,10)),
+        legend.text=element_text(size=20),
+        #legend.title=element_text(size=22),
         panel.grid.major = element_blank(),
         panel.grid.minor = element_blank(),
         panel.background = element_blank(),
         text = element_text(size = 12),
-        axis.text.x=element_text(size = 12, colour='black'),
+        axis.text.x=element_text(size = 15, colour='black'),
         axis.text.y=element_text(size = 12, colour='black'),
-        axis.title.y=element_text(size = 13),
-        axis.title.x=element_text(size = 13))
-  return(plot)
-}
-
-# Start plotting models with one hyper-parameter individually
-l1svm <- l1svm_all %>%
-  group_by(cost) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC))
-
-l1svm_plot <- base_plot(l1svm, l1svm$cost, l1svm$mean_AUC) +
-  scale_x_log10(name="C (penalty)") +
-  scale_y_continuous(name="L1 SVM with linear kernel mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=.001)
-
-
-l2svm <- l2svm_all %>%
-  group_by(cost) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC))
-
-l2svm_plot <- base_plot(l2svm, l2svm$cost, l2svm$mean_AUC) +
-  scale_x_log10(name="C (penalty)") +
-  scale_y_continuous(name="L2 SVM with linear kernel mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=.001)
-
-logit <- logit_all %>%
-  group_by(cost, loss, epsilon) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC)) 
-
-logit_plot <- base_plot(logit, logit$cost, logit$mean_AUC) + 
-  scale_x_log10(name="C (penalty)") +
-  scale_y_continuous(name="L2 logistic regression mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=.001) 
-
-
-dt <- dt_all %>%
-  group_by(maxdepth) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC))
-
-dt_plot <- base_plot(dt, dt$maxdepth, dt$mean_AUC) +
-scale_x_continuous(name="max depth") +
-  scale_y_continuous(name="Decision tree mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=.001)
-
-rf <- rf_all %>%
-  group_by(mtry) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC))
-
-rf_plot <-  base_plot(rf, rf$mtry, rf$mean_AUC) +
-scale_x_continuous(name="mtry",
-                   breaks=seq(0, 1500, 250), limits = c(0, 1500)) +
-  scale_y_continuous(name="Random forest mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=1)
-
-# Start plotting models with 2 hyper-parameters individually
-
-rbf_data<- rbf_all %>%
-  group_by(sigma, C) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC)) 
-
-library(scales)
-rbf_data$sigma <- scientific(rbf_data$sigma)
-rbf_data$sigma <- as.character(rbf_data$sigma)
-
-  
-rbf_plot <- ggplot(rbf_data, aes(x=C, y=mean_AUC, color=sigma)) +
-  geom_line() +
-  geom_point() + 
-  scale_x_log10(breaks = c(0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10)) +
-  scale_color_brewer(palette="Dark2") +
-  scale_y_continuous(name="SVM with radial bias kernel mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=0.01)
-
-xgboost_data <- xgboost_all %>%
-  group_by(eta, subsample) %>%
-  summarise(mean_AUC = mean(ROC), sd_AUC = sd(ROC)) 
-
-
-xgboost_data$subsample <- as.character(xgboost_data$subsample)
-xgboost_plot <- ggplot(xgboost_data, aes(x=eta, y=mean_AUC, color=subsample)) +
-  geom_line() +
-  geom_point()+
-  scale_x_continuous(breaks = c(0.001, 0.005, 0.01, 0.015, 0.02)) +
-  scale_y_continuous(name="XGBoost mean cvAUC",
-                     limits = c(0.30, 1),
-                     breaks = seq(0.3, 1, 0.05)) +
-  scale_color_brewer(palette="Dark2") +
-  geom_errorbar(aes(ymin=mean_AUC-sd_AUC, ymax=mean_AUC+sd_AUC), width=0.00001)
-
-  
-  
-
-
-
-all <- plot_grid(logit_plot, l1svm_plot, l2svm_plot, rbf_plot, rf_plot, dt_plot, xgboost_plot, labels = c("A", "B", "C", "D", "E", "F", "G"))
+        axis.title.y=element_text(size = 30),
+        axis.title.x=element_text(size = 20), 
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) 
 
 ######################################################################
 #-----------------------Save figure as .pdf ------------------------ #
 ######################################################################
 
-ggsave("Figure_2.pdf", plot = all, device = 'pdf', path = 'results/figures', width = 20, height = 15)
+ggsave("Figure_2.pdf", plot = performance, device = 'pdf', path = 'results/figures', width = 15, height = 10)
+
