6474d65f7784c38fe5aaf6f06e5d6e2696a7f8dc
BegumTop <begumtop@umich.edu>
2019-04-25 10:38:41 -0400

    leftover with fit

--- code/cat_csv_files_test.sh
+++ code/cat_csv_files_test.sh
@@ -18,7 +18,7 @@ SEARCH_DIR=data/temp
 FINAL_DIR=data/process
 # Keep the first line of File1 and remove the first line of all the others and combine
 
-for model in "XGBoost" "Random_Forest" "Decision_Tree" "RBF_SVM" "L1_Linear_SVM" "L2_Logistic_Regression" "L2_Linear_SVM"
+for model in "Random_Forest" "XGBoost" "RBF_SVM"
 do
 	head -1 $SEARCH_DIR/all_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_all_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/all_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_all_hp_results_"$model".csv
 	head -1 $SEARCH_DIR/best_hp_results_"$model"_1.csv  > $SEARCH_DIR/combined_best_hp_results_"$model".csv; tail -n +2 -q $SEARCH_DIR/best_hp_results_"$model"_*.csv >> $SEARCH_DIR/combined_best_hp_results_"$model".csv
