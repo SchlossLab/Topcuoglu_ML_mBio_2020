42fb9ba999ea12eef917f5c098ef418ea64c2eef
Begum <topcuoglu.begum@gmail.com>
2019-06-05 13:55:51 -0400

    update extension

--- code/merge_feature_ranks.sh
+++ code/merge_feature_ranks.sh
@@ -12,11 +12,11 @@ FINAL_DIR=data/process
 
 # 1. Keep the first line of File0 and remove the first line of all the other files (File[0-99]) and
 #		output it to the FINAL_DIR location
-cp $SEARCH_DIR/"$MODEL"_"$DATA"_1.csv $FINAL_DIR/combined_"$MODEL"_"$DATA".csv
+cp $SEARCH_DIR/"$MODEL"_"$DATA"_1.tsv $FINAL_DIR/combined_"$MODEL"_"$DATA".tsv
 
 #	2. Append the other files to the end, but we want to be sure to ignore the 0 file since we don't
 #		want it printed twice
 #        "tail -n +2" makes tail print lines from 2nd line to the end
 #        "-q" tells it to not print the header with the file name
 #        ">>" adds all the tail stuff from every file to the combined file
-tail -n +2 -q $SEARCH_DIR/"$MODEL"_"$DATA"_{1..300}.csv >> $FINAL_DIR/combined_"$MODEL"_"$DATA".csv
+tail -n +2 -q $SEARCH_DIR/"$MODEL"_"$DATA"_{1..300}.tsv >> $FINAL_DIR/combined_"$MODEL"_"$DATA".tsv
