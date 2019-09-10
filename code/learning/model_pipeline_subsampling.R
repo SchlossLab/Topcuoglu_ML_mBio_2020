cbdf33e196b3e0f98e0388fb2329f565b1e6ef7c
Begum <topcuoglu.begum@gmail.com>
2019-09-10 14:23:39 -0400

    rename functions for when we want to do subsampling

---
+++ code/learning/model_pipeline_subsampling.R
@@ -0,0 +1,193 @@
+
+# Author: Begum Topcuoglu
+# Date: 2019-01-14
+######################################################################
+# Description:
+# This script trains and tests the model according to proper pipeline
+######################################################################
+
+######################################################################
+# Dependencies and Outputs:
+#    Model to put to function:
+#       1. "L2_Logistic_Regression"
+#       2. "L2_Linear_SVM"
+#       3. "RBF_SVM"
+#       4. "Decision_Tree"
+#       5. "Random_Forest"
+#       6. "XGBoost"
+#    Dataset to put to function:
+#         Features: Hemoglobin levels and 16S rRNA gene sequences in the stool
+#         Labels: - Colorectal lesions of 490 patients.
+#                 - Defined as cancer or not.(Cancer here means: SRN)
+#
+# Usage:
+# Call as source when using the function. The function is:
+#   pipeline(data, model)
+
+# Output:
+#  A results list of:
+#     1. cvAUC and testAUC for 1 data-split
+#     2. cvAUC for all hyper-parameters during tuning for 1 datasplit
+#     3. feature importance info on first 10 features for 1 datasplit
+#     4. trained model as a caret object
+######################################################################
+
+######################################################################
+#------------------------- DEFINE FUNCTION -------------------#
+######################################################################
+
+
+pipeline <- function(dataset, model, split_number, subsample_number, subsample_name){
+
+  # ------------------Pre-process the full Dataset------------------------->
+  # We are doing the pre-processing to the full dataset and then splitting 80-20
+  # Scale all features between 0-1
+  preProcValues <- preProcess(dataset, method = "range")
+  dataTransformed <- predict(preProcValues, dataset)
+  # ----------------------------------------------------------------------->
+
+  # ------------------80-20 Datasplit for each seed------------------------->
+  # Do the 80-20 data-split
+  # Stratified data partitioning %80 training - %20 testing
+  inTraining <- createDataPartition(dataTransformed$dx, p = .80, list = FALSE)
+  trainTransformed <- dataTransformed[ inTraining,]
+  testTransformed  <- dataTransformed[-inTraining,]
+  # ----------------------------------------------------------------------->
+
+  # -------------Define hyper-parameter and cv settings-------------------->
+  # Define hyper-parameter tuning grid and the training method
+  # Uses function tuning_grid() in file ('code/learning/model_selection.R')
+  grid <- tuning_grid(trainTransformed, model)[[1]]
+  method <- tuning_grid(trainTransformed, model)[[2]]
+  cv <- tuning_grid(trainTransformed, model)[[3]]
+  # ----------------------------------------------------------------------->
+
+  # ---------------------------Train the model ---------------------------->
+  # ------------------------------- 1. -------------------------------------
+  # - We train on the 80% of the full dataset.
+  # - We use the cross-validation and hyper-parameter settings defined above to train
+  # ------------------------------- 2. -------------------------------------
+  # We use ROC metric for all the models
+  # To do that I had to make changes to the caret package functions.
+  # The files 'data/caret_models/svmLinear3.R and svmLinear5.R are my functions.
+  # I added 1 line to get Decision Values for linear SVMs:
+  #
+  #           prob = function(modelFit, newdata, submodels = NULL){
+  #             predict(modelFit, newdata, decisionValues = TRUE)$decisionValues
+  #           },
+  #
+  # This line gives decision values instead of probabilities and computes ROC in:
+  #   1. train function with the cross-validataion
+  #   2. final trained model
+  # using decision values and saves them in the variable "prob"
+  # ------------------------------- 3. --------------------------------------
+  # - If the model is logistic regression, we need to add a family=binomial parameter.
+  # - If the model is random forest, we need to add a ntree=1000 parameter.
+  #         We chose ntree=1000 empirically.
+  # ----------------------------------------------------------------------->
+  # Start walltime for training model
+  tic("train")
+  if(model=="L2_Logistic_Regression"){
+  print(model)
+  trained_model <-  train(dx ~ ., # label
+                          data=trainTransformed, #total data
+                          method = method,
+                          trControl = cv,
+                          metric = "ROC",
+                          tuneGrid = grid,
+                          family = "binomial")
+  }
+  else if(model=="Random_Forest"){
+      print(model)
+      trained_model <-  train(dx ~ .,
+                              data=trainTransformed,
+                              method = method,
+                              trControl = cv,
+                              metric = "ROC",
+                              tuneGrid = grid,
+                              ntree=1000) # not tuning ntree
+  }
+  else{
+    print(model)
+    trained_model <-  train(dx ~ .,
+                            data=trainTransformed,
+                            method = method,
+                            trControl = cv,
+                            metric = "ROC",
+                            tuneGrid = grid)
+  }
+  # Stop walltime for running model
+  seconds <- toc()
+  # Save elapsed time
+  train_time <- seconds$toc-seconds$tic
+  # Save wall-time
+  write.csv(train_time, file=paste0("data/temp/traintime_", model, "_", 
+                                    subsample_name, "_",
+                                    split_number, ".csv"), row.names=F)
+  # ------------- Output the cvAUC and testAUC for 1 datasplit ---------------------->
+  # Mean cv AUC value over repeats of the best cost parameter during training
+  cv_auc <- getTrainPerf(trained_model)$TrainROC
+  # Save all results of hyper-parameters and their corresponding meanAUCs over 100 internal repeats
+  results_individual <- trained_model$results
+  # ---------------------------------------------------------------------------------->
+
+  # -------------------------- Feature importances ----------------------------------->
+  #   Output the weights of features of linear models
+  #   Output the feature importances based on random permutation for non-linear models
+  # Here we look at the top 10 important features
+  if(model=="L1_Linear_SVM" || model=="L2_Linear_SVM" || model=="L2_Logistic_Regression"){
+    if(subsample_number==1){
+    # We will use the permutation_importance function here to:
+    #     1. Predict held-out test-data
+    #     2. Calculate ROC and AUROC values on this prediction
+    #     3. Get the feature importances for correlated and uncorrelated feautures
+    roc_results <- permutation_importance(trained_model, testTransformed)
+    test_auc <- roc_results[[1]]
+    # Predict the test importance
+    feature_importance_non_cor <- roc_results[2]
+    # Get feature weights
+    feature_importance_cor <- trained_model$finalModel$W
+    }
+    else{
+      print("We are doing a subsampling experiment. No need for permutation importance with lower number samples!")
+      # Get feature weights
+      feature_importance_non_cor <- trained_model$finalModel$W
+      # Get feature weights
+      feature_importance_cor <- trained_model$finalModel$W
+      # Calculate the test-auc for the actual pre-processed held-out data
+      rpartProbs <- predict(trained_model, testTransformed, type="prob")
+      test_roc <- roc(ifelse(testTransformed$dx == "cancer", 1, 0), rpartProbs[[1]])
+      test_auc <- test_roc$auc
+    }
+  }
+  else{
+    if(subsample_number==1){
+      # We will use the permutation_importance function here to:
+      #     1. Predict held-out test-data
+      #     2. Calculate ROC and AUROC values on this prediction
+      #     3. Get the feature importances for correlated and uncorrelated feautures
+      roc_results <- permutation_importance(trained_model, testTransformed)
+      test_auc <- roc_results[[1]]
+      # Predict the test importance
+      feature_importance_non_cor <- roc_results[2]
+      feature_importance_cor <- roc_results[3]
+    }
+    else{
+      print("We are doing a subsampling experiment. No need for permutation importance with lower number samples!")
+      # Get feature weights
+      feature_importance_non_cor <- NULL
+      # Get feature weights
+      feature_importance_cor <- NULL
+      # Calculate the test-auc for the actual pre-processed held-out data
+      rpartProbs <- predict(trained_model, testTransformed, type="prob")
+      test_roc <- roc(ifelse(testTransformed$dx == "cancer", 1, 0), rpartProbs[[1]])
+      test_auc <- test_roc$auc
+    }
+  }
+  # ---------------------------------------------------------------------------------->
+
+  # ----------------------------Save metrics as vector ------------------------------->
+  # Return all the metrics
+  results <- list(cv_auc, test_auc, results_individual, feature_importance_non_cor, feature_importance_cor, trained_model)
+  return(results)
+}
---
+++ code/learning/model_selection_subsampling.R
@@ -0,0 +1,178 @@
+# Author: Begum Topcuoglu
+# Date: 2019-01-14
+######################################################################
+# Description:
+# This function defines:
+#     1. Tuning budget as a grid for the classification methods chosen
+#     2. Cross-validation method (how many repeats and folds)
+#     3. Caret name for the classification method chosen
+######################################################################
+
+######################################################################
+# Dependencies and Outputs:
+#    Filenames to put to function:
+#       1. "L2_Logistic_Regression"
+#       2. "L2_Linear_SVM"
+#       3. "RBF_SVM"
+#       4. "Decision_Tree"
+#       5. "Random_Forest"
+#       6. "XGBoost"
+
+# Usage:
+# Call as source when using the function. The function is:
+#   tuning_grid()
+
+# Output:
+#  List of:
+#     1. Tuning budget as a grid the classification methods chosen
+#     2. Cross-validation method
+#     3. Caret name for the classification method chosen
+######################################################################
+
+
+######################################################################
+#------------------------- DEFINE FUNCTION -------------------#
+######################################################################
+tuning_grid <- function(train_data, model){
+
+
+# -------------------------CV method definition--------------------------------------->
+# ADDED cv index to make sure
+#     1. the internal 5-folds are stratified for diagnosis classes
+#     2. Resample the dataset 100 times for 5-fold cv to get robust hp.
+# IN trainControl function:
+#     1. Train the model with final hp decision to use model to predict
+#     2. Return 2class summary and save predictions to calculate cvROC
+#     3. Save the predictions and class probabilities/decision values.
+  folds <- 5
+  cvIndex <- createMultiFolds(factor(train_data$dx), folds, times=100)
+  cv <- trainControl(method="repeatedcv",
+                     number=folds,
+                     index = cvIndex,
+                     returnResamp="final",
+                     classProbs=TRUE,
+                     summaryFunction=twoClassSummary,
+                     indexFinal=NULL,
+                     savePredictions = TRUE)
+# # ----------------------------------------------------------------------->
+
+
+
+# -------------------Classification Method Definition---------------------->
+
+# ---------------------------------1--------------------------------------->
+# For linear models we are using LiblineaR package
+#
+# LiblineaR can produce 10 types of (generalized) linear models:
+# The regularization can be
+#     1. L1
+#     2. L2
+# The losses can be:
+#     1. Regular L2-loss for SVM (hinge loss),
+#     2. L1-loss for SVM
+#     3. Logistic loss for logistic regression.
+#
+# Here we will use L1 and L2 regularization and hinge loss (L2-loss) for linear SVMs
+# We will use logistic loss for L2-resularized logistic regression
+# The liblinear 'type' choioces are below:
+#
+# for classification
+# • 0 – L2-regularized logistic regression (primal)---> we use this for l2-logistic
+#  • 1 – L2-regularized L2-loss support vector classification (dual)
+#  • 2 – L2-regularized L2-loss support vector classification (primal) ---> we use this for l2-linear SVM
+#  • 3 – L2-regularized L1-loss support vector classification (dual)
+#  • 4 – support vector classification by Crammer and Singer
+#  • 5 – L1-regularized L2-loss support vector classification---> we use this for l1-linear SVM
+#  • 6 – L1-regularized logistic regression
+#  • 7 – L2-regularized logistic regression (dual)
+#
+#for regression
+#  • 11 – L2-regularized L2-loss support vector regression (primal)
+#  • 12 – L2-regularized L2-loss support vector regression (dual)
+#  • 13 – L2-regularized L1-loss support vector regression (dual)
+# ------------------------------------------------------------------------>
+
+
+  # ---------------------------------2--------------------------------------->
+  # We use ROC metric for all the models
+  # To do that I had to make changes to the caret package functions.
+  # The files 'data/caret_models/svmLinear3.R and svmLinear5.R are my functions.
+  # I added 1 line to get Decision Values for linear SVMs:
+  #
+  #           prob = function(modelFit, newdata, submodels = NULL){
+  #             predict(modelFit, newdata, decisionValues = TRUE)$decisionValues
+  #           },
+  #
+  # This line gives decision values instead of probabilities and computes ROC in:
+  #   1. train function with the cross-validataion
+  #   2. final trained model
+  # using decision values and saves them in the variable "prob"
+  # This allows us to pass the cv function for all models:
+  # cv <- trainControl(method="repeatedcv",
+  #                   repeats = 100,
+  #                   number=folds,
+  #                   index = cvIndex,
+  #                   returnResamp="final",
+  #                   classProbs=TRUE,
+  #                   summaryFunction=twoClassSummary,
+  #                   indexFinal=NULL,
+  #                   savePredictions = TRUE)
+  #
+  # There parameters we pass for L1 and L2 SVM:
+  #                   classProbs=TRUE,
+  #                   summaryFunction=twoClassSummary,
+  # are actually computing ROC from decision values not probabilities
+  # ------------------------------------------------------------------------>
+
+  # Grid and caret method defined for each classification models
+  if(model=="L2_Logistic_Regression") {
+    grid <-  expand.grid(cost = c(0.0001, 0.001, 0.0025, 0.005, 0.01, 0.05, 0.1, 0.25, 0.5, 1, 10),
+                         loss = "L2_primal",
+                         # This chooses type=0 for liblinear R package
+                         # which is logistic loss, primal solve for L2 regularized logistic regression
+                         epsilon = 0.01) #default epsilon recommended from liblinear
+    method <- "regLogistic"
+  }
+  else if (model=="L1_Linear_SVM"){ #
+    grid <- expand.grid(cost = c(0.0001, 0.001, 0.01, 0.015, 0.025, 0.05, 0.1, 0.5, 1),
+                        Loss = "L2")
+    method <- "svmLinear4" # I wrote this function in caret
+  }
+  else if (model=="L2_Linear_SVM"){
+    grid <- expand.grid(cost = c(0.0001, 0.0005, 0.001, 0.0025, 0.005, 0.01, 0.025, 0.05, 0.1, 0.5, 1),
+                        Loss = "L2")
+    method <- "svmLinear3" # I changed this function in caret
+  }
+  else if (model=="RBF_SVM"){
+    grid <-  expand.grid(sigma = c(0.00000001, 0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1),
+                         C = c(0.0000001, 0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10))
+    method <-"svmRadial"
+  }
+  else if (model=="Decision_Tree"){
+    grid <-  expand.grid(maxdepth = c(1,2,3,4,5,6))
+    method <-"rpart2"
+  }
+  else if (model=="Random_Forest"){
+    grid <-  expand.grid(mtry = c(80,500,1000,1500))
+    method = "rf"
+  }
+  else if (model=="XGBoost"){
+    grid <-  expand.grid(nrounds=500,
+                         gamma=0,
+                         eta=c(0.001, 0.01, 0.1, 1),
+                         max_depth=8,
+                         colsample_bytree= 0.8,
+                         min_child_weight=1,
+                         subsample=c(0.4, 0.5, 0.6, 0.7))
+    method <- "xgbTree"
+  }
+  else {
+    print("Model not available")
+  }
+  # Return:
+  #     1. the hyper-parameter grid to tune
+  #     2. the caret function to train with
+  #     3, cv method
+  params <- list(grid, method, cv)
+  return(params)
+}
